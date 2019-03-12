96f6acc4545232fa7c240dcefb55b97fd431858c
pointbaaar <alex23667@gmail.com>
2019-03-13 00:27:09 +0100

    change access level to token contents

--- src/main/java/org/vanautrui/languages/model/AmandaToken.java
+++ src/main/java/org/vanautrui/languages/model/AmandaToken.java
@@ -2,12 +2,16 @@
 package org.vanautrui.languages.model;
 
 public class AmandaToken {
-    public String contents;
+    private String contents;
 
     public AmandaToken(String newcontents) throws Exception{
         if(newcontents==null || newcontents.isEmpty()){
             throw new Exception("Trying to create empty token");
         }
         this.contents=newcontents;
+    }
+
+    public String getContents(){
+        return new String(contents.getBytes());
     }
 }
