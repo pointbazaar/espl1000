3ced327555fe897def469c0efe474f87838aa0f0
pointbazaar <alex23667@gmail.com>
2019-07-21 23:48:21 +0200

    improve the command

--- .dragon.sh
+++ .dragon.sh
@@ -4,6 +4,11 @@
 
 function dragon(){
 	echo "dragon launched. "
-	echo $1
-	java -jar ~/dragon/target/dragon-0.1.jar
+	#echo $1
+
+
+	#https://www.devdungeon.com/content/taking-command-line-arguments-bash
+	#https://stackoverflow.com/questions/456636/how-do-i-pass-parameters-to-a-jar-file-at-the-time-of-execution
+
+	java -jar ~/dragon/target/dragon-0.1.jar "$@"
 }
