600132cd9bb7d4580fdbb79d6c024ab43fc49d73
pointbazaar <alex23667@gmail.com>
2019-08-01 23:14:24 +0200

    add a project tree

--- buildrun.sh
+++
@@ -1,4 +0,0 @@
-mvn install -DskipTests
-#java -jar target/amanda-0.1.jar -i
-mvn exec::java
-
--- src/main/java/org/vanautrui/languages/editor/DragonGUI_Editor.java
+++ src/main/java/org/vanautrui/languages/editor/DragonGUI_Editor.java
@@ -1,6 +1,7 @@
 package org.vanautrui.languages.editor;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 
 public class DragonGUI_Editor {
@@ -17,8 +18,8 @@ public class DragonGUI_Editor {
         this.frame = new JFrame("Dragon Editor");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(default_width,default_height);
-
+        //frame.setSize(default_width,default_height);
+        frame.setMinimumSize(new Dimension(default_width,default_height));
         frame.setResizable(false);
 
         JButton button = new JButton("Button");
@@ -27,7 +28,7 @@ public class DragonGUI_Editor {
 
         frame.getContentPane().add(BorderLayout.NORTH,new JButton("tools panel"));
 
-        frame.getContentPane().add(BorderLayout.WEST,new JButton("project tree"));
+        frame.getContentPane().add(BorderLayout.WEST,projectArea());
 
         frame.getContentPane().add(BorderLayout.SOUTH,new JButton("status bar"));
 
@@ -40,9 +41,28 @@ public class DragonGUI_Editor {
 
     }
 
+    private JTree projectArea(){
+        DefaultMutableTreeNode treeNode = new DefaultMutableTreeNode();
+
+        DefaultMutableTreeNode child1 = new DefaultMutableTreeNode(
+                "node 1"
+        );
+
+        DefaultMutableTreeNode child1_1 = new DefaultMutableTreeNode(
+                "node 1 . 1"
+        );
+
+        treeNode.add(child1);
+        child1.add(child1_1);
+
+        JTree tree = new JTree(treeNode);
+        return tree;
+    }
+
     private Component editorArea(){
         TextArea textArea = new TextArea();
         textArea.setPreferredSize(new Dimension(400,500));
+        textArea.setMinimumSize(new Dimension(500,500));
         return textArea;
     }
 
