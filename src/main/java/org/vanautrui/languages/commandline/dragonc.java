e881e7e724db8c980b372ec40d8eaf2b04ef766a
pointbazaar <alex23667@gmail.com>
2019-08-01 19:58:05 +0200

    remove unneccessary whitespace, to prettiy and because David asked for it

--- src/main/java/org/vanautrui/languages/commandline/dragonc.java
+++ src/main/java/org/vanautrui/languages/commandline/dragonc.java
@@ -11,6 +11,8 @@ import org.vanautrui.languages.parsing.astnodes.nonterminal.DragonAST;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class dragonc {
     //this should be the compiler
@@ -46,14 +48,20 @@ public class dragonc {
 
                 TerminalUtil.printlnRed("PHASE: REMOVE COMMENTS AND EMPTY LINES");
 
-                String codeWithoutComments = (new DragonCommentRemover()).strip_comments(sourceCode);
+                String codeWithoutCommentsAndWithoutEmptyLines = (new DragonCommentRemover()).strip_comments(sourceCode);
 
-                System.out.println(codeWithoutComments);
+                System.out.println(codeWithoutCommentsAndWithoutEmptyLines);
+
+                //maybe phase to remove unneccessary whitespace?
+                TerminalUtil.printlnRed("TODO: PHASE: REMOVE UNNECCESSARY WHITESPACE");
+                String codeWithoutCommentsWithoutUnneccesaryWhitespace = remove_unneccessary_whitespace(codeWithoutCommentsAndWithoutEmptyLines);
+
+                System.out.println(codeWithoutCommentsWithoutUnneccesaryWhitespace);
 
                 TerminalUtil.printlnRed("PHASE: WEAVE IN CURLY BRACES");
 
                 String just_code_with_braces_without_comments_without_newlines =
-                        CurlyBracesWeaver.weave_scoping_curly_braces_and_remove_newlines(codeWithoutComments);
+                        CurlyBracesWeaver.weave_scoping_curly_braces_and_remove_newlines(codeWithoutCommentsWithoutUnneccesaryWhitespace);
 
                 System.out.println(just_code_with_braces_without_comments_without_newlines);
 
@@ -93,5 +101,71 @@ public class dragonc {
         } else {
             System.err.println("Please specify some files, like 'dragon -c main.dragon' ");
         }
+    }
+
+    private static String remove_unneccessary_whitespace(String codeWithoutCommentsAndWithoutEmptyLines) {
+
+        String result = Arrays.stream(codeWithoutCommentsAndWithoutEmptyLines.split("\n"))
+                .map(
+                        str->remove_unneccessary_whitespace_line(str)
+                )
+                .collect(Collectors.joining("\n"));
+
+        return result;
+    }
+
+    private static String remove_unneccessary_whitespace_line(String str) {
+        //the initial whitespace, and whitespace within strings should remain
+        //1 whitespace between words should remain
+
+        StringBuilder sb=new StringBuilder("");
+
+        //TODO: save the indentation from being removed
+
+        boolean last_was_whitespace=true;
+        boolean inside_string=false;
+        boolean start_of_line=false;
+
+        int i=0;
+
+        while(i<str.length() && !start_of_line){
+            char c = str.charAt(i);
+            switch (c) {
+                case '\t':
+                case ' ':
+                    sb.append(c);
+                    i++;
+                    break;
+                default:
+                    start_of_line = true;
+            }
+        }
+
+        while(i<str.length()){
+            char c=str.charAt(i);
+
+            switch (c){
+                case '\"':
+                    inside_string=!inside_string;
+                    last_was_whitespace=false;
+                    sb.append(c);
+                    break;
+                case ' ':
+                    if(last_was_whitespace && !inside_string){
+                        //we skip it
+                    }else{
+                        sb.append(c);
+                    }
+                    last_was_whitespace=true;
+                    break;
+                default:
+                    last_was_whitespace=false;
+                    sb.append(c);
+                    break;
+            }
+            i++;
+        }
+
+        return sb.toString();
     }
 }
