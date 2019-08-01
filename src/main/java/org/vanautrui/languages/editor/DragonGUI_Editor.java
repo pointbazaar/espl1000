51ea5b7337745fbd0b1a92fb9517d044059adbf5
pointbazaar <alex23667@gmail.com>
2019-08-01 23:56:31 +0200

    make a splash screen

--- src/main/java/org/vanautrui/languages/editor/DragonGUI_Editor.java
+++ src/main/java/org/vanautrui/languages/editor/DragonGUI_Editor.java
@@ -1,16 +1,22 @@
 package org.vanautrui.languages.editor;
 
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class DragonGUI_Editor {
 
     //https://www.guru99.com/java-swing-gui.html
 
-    public static final int default_width=600;
+    public static final int default_width=800;
     public static final int default_height=300;
 
     private JFrame frame;
@@ -23,23 +29,47 @@ public class DragonGUI_Editor {
         //frame.setSize(default_width,default_height);
         frame.setMinimumSize(new Dimension(default_width,default_height));
         frame.setResizable(false);
+        Point p = new Point(300,300);
+        frame.setLocation(p);
 
-        JButton button = new JButton("Button");
-
-        //frame.getContentPane().add(button);
+        JLabel splash = splashScreen();
+        frame.getContentPane().add(splash);
+        frame.setVisible(true);
+        try{
+            Thread.sleep(800);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        frame.getContentPane().remove(splash);
 
         frame.getContentPane().add(BorderLayout.NORTH, menuBar());
-
         frame.getContentPane().add(BorderLayout.WEST,projectArea());
-
-        frame.getContentPane().add(BorderLayout.SOUTH,new JButton("status bar"));
-
+        frame.getContentPane().add(BorderLayout.SOUTH,statusBar());
         frame.getContentPane().add(BorderLayout.CENTER,editorArea());
-
         frame.getContentPane().add(BorderLayout.EAST,new JButton("context information, file navigation"));
 
         frame.setVisible(true);
 
+
+    }
+
+    private Component statusBar(){
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("TODO: status bar"));
+        panel.add(new JLabel("TODO: line number"));
+        return panel;
+    }
+
+    private JLabel splashScreen(){
+        System.out.println("making splash screen");
+        try {
+            BufferedImage splash = ImageIO.read(new File("splashscreens/programming.jpeg"));
+            JLabel picLabel = new JLabel(new ImageIcon(splash));
+            return picLabel;
+        }catch (Exception e){
+            e.printStackTrace();
+            return new JLabel("could not read image "+e.getMessage());
+        }
 
     }
 
