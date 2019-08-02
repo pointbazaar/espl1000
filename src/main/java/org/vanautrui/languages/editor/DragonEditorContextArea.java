d286a4ccb8368269a4ddfea9304b88000e02c7f0
pointbazaar <alex23667@gmail.com>
2019-08-03 00:19:33 +0200

    learn a bit about textarea

--- src/main/java/org/vanautrui/languages/editor/DragonEditorContextArea.java
+++ src/main/java/org/vanautrui/languages/editor/DragonEditorContextArea.java
@@ -11,10 +11,21 @@ public class DragonEditorContextArea {
     private DragonGUI_Editor master;
 
     public DragonEditorContextArea(DragonGUI_Editor master1){
+
+        //https://docs.oracle.com/javase/tutorial/uiswing/components/textarea.html
+
         this.master=master1;
         this.available_completions=new JTextArea();
-        this.available_completions.setMinimumSize(new Dimension(200,200));
+        //this.available_completions.setMinimumSize(new Dimension(200,200));
         this.available_completions.add(new JLabel("completions will appear here"));
+
+
+        //this.available_completions.setMaximumSize(new Dimension(200,100));
+        this.available_completions.setLineWrap(false);
+        this.available_completions.setEditable(false);
+
+        this.available_completions.setRows(7);
+        this.available_completions.setColumns(18);
     }
 
     public Component make(){
