b8507a9b61cf5836426e9590cbb967e602769fc6
pointbazaar <alex23667@gmail.com>
2019-08-04 04:50:15 +0200

    try to make the action stuff right, but it seems to look at the physical key location

--- src/main/java/org/vanautrui/languages/editor/DragonKeyEventHandler.java
+++ src/main/java/org/vanautrui/languages/editor/DragonKeyEventHandler.java
@@ -2,8 +2,12 @@ package org.vanautrui.languages.editor;
 
 import javafx.scene.input.KeyCode;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DragonKeyEventHandler {
 
@@ -11,8 +15,19 @@ public class DragonKeyEventHandler {
 
     private DragonGUI_Editor master;
 
+    private Map<KeyStroke,Action> actionMap = new HashMap<>();
+
     public DragonKeyEventHandler(DragonGUI_Editor master){
         this.master=master;
+
+        KeyStroke copy = KeyStroke.getKeyStroke(KeyEvent.VK_C,KeyEvent.CTRL_DOWN_MASK);
+
+        actionMap.put(copy, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("CTRL+C (COPY) Pressed");
+            }
+        });
     }
 
     public KeyEventDispatcher getKeyEventDispatcher(){
@@ -27,6 +42,25 @@ public class DragonKeyEventHandler {
                 System.out.println("-----------------");
                 //System.out.println(e.toString());
                 //System.out.println(e.paramString());
+
+                //TODO: this refers to the physical key. if you are using another layout, it doesnt work. fix it.
+                KeyEvent myKeyEvent = new KeyEvent(master.contextArea.get().make(),0,0,e.getModifiers(),e.getExtendedKeyCode());
+
+                KeyStroke keyStroke = KeyStroke.getKeyStrokeForEvent(myKeyEvent);
+
+                if ( actionMap.containsKey(keyStroke) ) {
+                    final Action a = actionMap.get(keyStroke);
+                    final ActionEvent ae = new ActionEvent(e.getSource(), e.getID(), null );
+                    SwingUtilities.invokeLater( new Runnable() {
+                        @Override
+                        public void run() {
+                            a.actionPerformed(ae);
+                        }
+                    } );
+                }
+
+
+
                 String key_event_type = e.paramString().split(",")[0];
 
                 switch (key_event_type){
@@ -47,14 +81,19 @@ public class DragonKeyEventHandler {
                         break;
                 }
                 System.out.println(key_event_type);
+                //extended keycode seems to represent the key we want
+                System.out.println("extended keycode: "+e.getExtendedKeyCode());
 
-                System.out.println(e.getKeyCode());
+                //keycode seems to r
+                System.out.println("keycode: "+e.getKeyCode());
                 System.out.println("key dispatch: "+e.getKeyChar());
 
                 //debug
-                master.editorWithImage.get().appendLineTest();
+                //master.editorWithImage.get().appendLineTest();
 
                 return true;
+
+
             }
         };
         return keyEventDispatcher;
