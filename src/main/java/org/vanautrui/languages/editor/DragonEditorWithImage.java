1715325f1403040659effbd8a573326ed7593b94
pointbazaar <alex23667@gmail.com>
2019-08-06 00:32:27 +0200

    rip out some dead code and add a TODO to write tests for the text editing logic

--- src/main/java/org/vanautrui/languages/editor/DragonEditorWithImage.java
+++ src/main/java/org/vanautrui/languages/editor/DragonEditorWithImage.java
@@ -14,6 +14,10 @@ import java.util.List;
 
 public class DragonEditorWithImage {
 
+    //TODO: add some unit tests, for the text editing code
+    //so that new text editing functionality
+    //can be added in confidence of not breaking anything
+
     private JPanel panel;
 
     private DragonGUI_Editor master;
@@ -33,11 +37,7 @@ public class DragonEditorWithImage {
 
 
         addLine(0,"");
-        try {
-            this.updateJLabelOnLine(0);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        this.updateEditorImage();
 
         this.panel.setBorder(BorderFactory.createLineBorder(Color.white));
 
@@ -51,15 +51,12 @@ public class DragonEditorWithImage {
 
     }
 
-    //-----------
-    //to manage the components and lines in editor at the same time
     private void addLine(int beforeIndex,String text){
         this.lines_in_editor.add(beforeIndex,text);
     }
     private void removeLine(int index){
         this.lines_in_editor.remove(index);
     }
-    //--------------
 
 
     private void adjustCursorColumnToBeContainedInLine(){
@@ -73,12 +70,8 @@ public class DragonEditorWithImage {
         if(this.cursor_col>0){
             this.cursor_col--;
         }
-        try{
-            this.updateJLabelOnLine(this.cursor_line);
-            //this.updateCompletely();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+
+        this.updateEditorImage();
     }
 
     public void arrowRightMoveCursor(){
@@ -86,40 +79,21 @@ public class DragonEditorWithImage {
         this.cursor_col++;
         this.adjustCursorColumnToBeContainedInLine();
 
-        try{
-            this.updateJLabelOnLine(this.cursor_line);
-            //this.updateCompletely();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        this.updateEditorImage();
     }
 
     public void arrowUpChangeLine(){
         if(this.cursor_line>0){
             this.cursor_line--;
             this.adjustCursorColumnToBeContainedInLine();
-
-            try {
-                this.updateJLabelOnLine(this.cursor_line);
-                this.updateJLabelOnLine(this.cursor_line + 1);
-                //this.updateCompletely();
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+            this.updateEditorImage();
         }
     }
     public void arrowDownChangeLine(){
         if(this.cursor_line < this.lines_in_editor.size()-1){
             this.cursor_line++;
             this.adjustCursorColumnToBeContainedInLine();
-
-            try {
-                this.updateJLabelOnLine(this.cursor_line-1);
-                this.updateJLabelOnLine(this.cursor_line);
-                //this.updateCompletely();
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+            this.updateEditorImage();
         }
     }
 
@@ -147,27 +121,10 @@ public class DragonEditorWithImage {
                 this.stringBeforeCursor()+c+this.stringAfterCursor()
         );
         this.cursor_col++;
-
-        updateJLabelOnLine(this.cursor_line);
-        //this.updateCompletely();
+        updateEditorImage();
     }
 
-    private void updateCompletely()throws Exception{
-        long start = System.currentTimeMillis();
-
-        this.panel.removeAll();
-        for(int i=0;i<this.lines_in_editor.size();i++){
-            String s = this.lines_in_editor.get(i);
-            this.panel.add(LineImageService.makeImageForLine(s,i,this.cursor_line,this.cursor_col));
-        }
-        this.panel.updateUI();
-
-        long end=System.currentTimeMillis();
-
-        System.out.println("update took: "+(end-start)+" ms");
-    }
-
-    private void updateJLabelOnLine(int line) throws Exception{
+    private void updateEditorImage(){
 
         long start = System.currentTimeMillis();
         this.panel.removeAll();
@@ -177,31 +134,12 @@ public class DragonEditorWithImage {
         long end = System.currentTimeMillis();
         System.out.println("complete editor draw took: "+(end-start)+" ms");
 
-        /*
-        MyImagePanel panel = (MyImagePanel)this.panel.getComponent(line);
-        panel.setImg(
-                LineImageService.makeImageForLineInner(this.lines_in_editor.get(line),line,cursor_line,cursor_col)
-        );
-
-        /*
-        if(this.lines_in_editor.size()>line) {
-            this.panel.add(LineImageService.makeImageForLine(this.lines_in_editor.get(line), line,cursor_line,cursor_col));
-            try {
-                this.panel.remove(line+1);
-            }catch (Exception e){
-                //
-            }
-        }
-
-         */
-
         this.panel.updateUI();
-
         this.panel.invalidate();
         this.panel.repaint();
     }
 
-    public void appendLineTest(){
+    public void ppm_format_experiment(){
         try {
             Runtime rt = Runtime.getRuntime();
             String line="A";
@@ -211,24 +149,13 @@ public class DragonEditorWithImage {
 
             InputStream out = pr.getInputStream();
             //String image_in_ppm_format = IOUtils.toString(out);
-
             //System.out.println(image_in_ppm_format);
 
             BufferedImage read = ImageIO.read(out);
-            //BufferedImage read = new BufferedImage(30,30, ImageStorage.ImageType.RGB);
             Image read2 = read.getScaledInstance(20,20,Image.SCALE_DEFAULT);
 
-            //TODO: figure out why this is not visible
             JLabel line_img = new JLabel(new ImageIcon(read2));
             line_img.setMinimumSize(new Dimension(20,20));
-
-            //this.panel.add(line_img);
-            this.panel.add(new MyImagePanel(read2));
-
-            //this.panel.setVisible(true);
-            this.panel.updateUI();
-
-            System.out.println("try to append line");
 
         }catch (Exception e){
             e.printStackTrace();
@@ -255,16 +182,6 @@ public class DragonEditorWithImage {
             this.cursor_line++;
             this.cursor_col=0;
 
-            //TODO: fix the bug where making a newline doesnt draw anew the lines thereafter
-
-            //when a new line is inserted, a naive approach would be to then
-            //update the drawings of all the subsequent lines
-
-            //another approach would be to cache the stuff from the cpp program and
-            //re-render the whole thing, but as most of it is cached it would not be a problem...
-
-            //another approach would be to manually manage the images of the lines
-            //when new lines get inserted. that would probably be most performant
         }else{
             //cursor is in the middle of a line
             //if in the middle of a line, split that line, increment cursor
@@ -282,14 +199,7 @@ public class DragonEditorWithImage {
             this.cursor_line++;
         }
 
-        //update the Images of the respective lines to reflect the changes
-        try {
-            updateJLabelOnLine(this.cursor_line - 1);
-            updateJLabelOnLine(this.cursor_line);
-            //this.updateCompletely();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        updateEditorImage();
     }
 
     public void pressBackSpace() {
@@ -301,22 +211,15 @@ public class DragonEditorWithImage {
                 //cursor_line !=0 , cursor_col==0
                 //cursor_line >0 ~> previous line exists
                 String previous_line = this.lines_in_editor.get(this.cursor_line-1);
+
                 String current_line = this.lines_in_editor.get(this.cursor_line);
 
                 this.lines_in_editor.set(this.cursor_line-1,previous_line+current_line);
 
                 this.removeLine(this.cursor_line);
-                //this.lines_in_editor.remove(this.cursor_line);
 
                 this.cursor_line--;
-                this.cursor_col=this.lines_in_editor.get(this.cursor_line).length();
-
-                try{
-                    updateJLabelOnLine(this.cursor_line);
-                    //updateJLabelOnLine(this.cursor_line+1);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
+                this.cursor_col=previous_line.length();
             }
         }else{
             //cursor_col !=0    should mean   cursor_col > 0
@@ -333,17 +236,7 @@ public class DragonEditorWithImage {
             this.lines_in_editor.set(this.cursor_line,edited_line);
 
             this.cursor_col--;
-
-            try{
-                updateJLabelOnLine(this.cursor_line);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
         }
-        try{
-            //this.updateCompletely();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        this.updateEditorImage();
     }
 }
