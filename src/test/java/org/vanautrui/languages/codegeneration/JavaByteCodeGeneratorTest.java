7a93846f545d2c87a63b778e8e94b8135740df0c
pointbazaar <alex23667@gmail.com>
2019-08-16 15:21:59 +0200

    add a test that it can compile loop statements

--- src/test/java/org/vanautrui/languages/codegeneration/JavaByteCodeGeneratorTest.java
+++ src/test/java/org/vanautrui/languages/codegeneration/JavaByteCodeGeneratorTest.java
@@ -15,6 +15,26 @@ import java.nio.file.Paths;
 
 public class JavaByteCodeGeneratorTest {
 
+    private Process compile_and_run_one_class_for_testing(String source,String classNameWithoutExtension) throws Exception{
+
+        DragonTokenList tokens = (new DragonLexer()).lexCodeWithoutComments(source);
+        DragonParser parser = new DragonParser();
+        DragonAST ast= parser.parse(tokens);
+
+        byte[] result = JavaByteCodeGenerator.generateByteCodeForClass(ast.classNodeList.toArray(new DragonClassNode[]{})[0]);
+        Path path = Paths.get(classNameWithoutExtension+".class");
+
+        Files.write(path,result);
+        Process pr = Runtime.getRuntime().exec("java "+classNameWithoutExtension);
+
+        pr.waitFor();
+
+        //maybe it is safe to delete already
+        Files.delete(path);
+
+        return pr;
+    }
+
     @Test
     public void test_can_compile_simple_helloworld()throws Exception{
         String source="public class MainTest1 { public Void main(){ println(\"Hello World!\"); }}";
@@ -40,5 +60,14 @@ public class JavaByteCodeGeneratorTest {
         Files.delete(path);
 
         Assert.assertEquals("Hello World!\n",output);
+    }
+
+    @Test
+    public void test_can_compile_loop_statements()throws Exception{
+        String source="public class MainTest2 { public Void main(){ loop 4 { print(\"1\"); } } }";
+        Process pr = compile_and_run_one_class_for_testing(source,"MainTest2");
+
+        Assert.assertEquals(0,pr.exitValue());
+        Assert.assertEquals("1111",IOUtils.toString(pr.getInputStream()));
     }
 }
