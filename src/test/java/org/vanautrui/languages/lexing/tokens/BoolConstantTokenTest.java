0017a026a13fc7141bdec3b555b785ce08f7ebdc
pointbazaar <alex23667@gmail.com>
2019-08-23 00:17:44 +0200

    test bool constant lexing and parsing

---
+++ src/test/java/org/vanautrui/languages/lexing/tokens/BoolConstantTokenTest.java
@@ -0,0 +1,39 @@
+package org.vanautrui.languages.lexing.tokens;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.vanautrui.languages.lexing.collections.CharacterList;
+
+public class BoolConstantTokenTest {
+
+    @Test
+    public void test_true() throws Exception {
+
+        CharacterList list = new CharacterList("true");
+
+        BoolConstantToken token = new BoolConstantToken(list);
+
+        Assert.assertEquals("true", token.getContents());
+    }
+
+    @Test
+    public void test_false() throws Exception {
+
+        CharacterList list = new CharacterList("false");
+        BoolConstantToken token = new BoolConstantToken(list);
+        Assert.assertEquals("false", token.getContents());
+    }
+
+    @Test
+    public void test_not_capitalized() throws Exception {
+
+        CharacterList list = new CharacterList("True");
+        try {
+            IntegerNonNegativeConstantToken token = new IntegerNonNegativeConstantToken(list);
+            Assert.fail();
+        }catch (Exception e){
+            //pass
+        }
+    }
+
+}
---
+++ src/test/java/org/vanautrui/languages/parsing/astnodes/terminal/BoolConstantNodeTest.java
@@ -0,0 +1,21 @@
+package org.vanautrui.languages.parsing.astnodes.terminal;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.vanautrui.languages.lexing.collections.CharacterList;
+import org.vanautrui.languages.lexing.collections.TokenList;
+import org.vanautrui.languages.lexing.tokens.BoolConstantToken;
+import org.vanautrui.languages.lexing.tokens.FloatNonNegativeConstantToken;
+import org.vanautrui.languages.lexing.tokens.OperatorToken;
+
+public class BoolConstantNodeTest {
+
+    @Test
+    public void test_parse_bool_constant_node() throws Exception {
+        TokenList list = new TokenList();
+        list.add(new BoolConstantToken(new CharacterList("true")));
+
+        BoolConstantNode node = new BoolConstantNode(list);
+        Assert.assertEquals(true,node.value);
+    }
+}
