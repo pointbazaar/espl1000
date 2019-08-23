39569ae47257de50e9c048b814b0409f409e365a
pointbazaar <alex23667@gmail.com>
2019-08-23 10:40:02 +0200

    reading and writing arrays seems to work now

--- src/main/java/org/vanautrui/languages/codegeneration/AssignmentStatementCodeGenerator.java
+++ src/main/java/org/vanautrui/languages/codegeneration/AssignmentStatementCodeGenerator.java
@@ -1,6 +1,7 @@
 package org.vanautrui.languages.codegeneration;
 
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.vanautrui.languages.parsing.astnodes.nonterminal.statements.AssignmentStatementNode;
 import org.vanautrui.languages.parsing.astnodes.nonterminal.upperscopes.ClassNode;
@@ -24,6 +25,36 @@ public class AssignmentStatementCodeGenerator {
              SubroutineSymbolTable subTable,
             boolean debug
     ) throws Exception{
+
+        //TODO: consider the type of the value we are assigning to
+        //if we are storing into an array we need other bytecodes
+        mv.visitLabel(new Label());
+
+        if(assignmentStatementNode.variableNode.indexOptional.isPresent()){
+            //assigning into an array at index
+            visitAssignVarIntoArray(cw,classNode,assignmentStatementNode,mv,methodNode,varTable,subTable,debug);
+        }else{
+            visitAssignVarIntoVar(cw,classNode,assignmentStatementNode,mv,methodNode,varTable,subTable,debug);
+        }
+
+    }
+
+    private static void visitAssignVarIntoVar(
+            ClassWriter cw,
+            ClassNode classNode,
+            AssignmentStatementNode assignmentStatementNode,
+            MethodVisitor mv,
+            MethodNode methodNode,
+            LocalVarSymbolTable varTable,
+            SubroutineSymbolTable subTable,
+            boolean debug
+    )throws Exception{
+        //x=3;y=4;
+        //x=y;
+
+        //arr1=arr2
+
+        //both values are primitive, or both are reference types
         int local_var_index = varTable.getIndexOfVariable(assignmentStatementNode.variableNode.name);
 
         //evaluate the expression and store the result in the local variable
@@ -32,17 +63,10 @@ public class AssignmentStatementCodeGenerator {
 
         String expressionInstanceType= TypeResolver.getTypeExpressionNode(assignmentStatementNode.expressionNode,methodNode,subTable,varTable);
 
-        //TODO: consider the type of the value we are assigning to
-        //if we are storing into an array we need other bytecodes
-
         switch(expressionInstanceType){
             case "Int":
             case "Bool":
-                if(assignmentStatementNode.variableNode.indexOptional.isPresent()){
-                    mv.visitIntInsn(IASTORE,local_var_index);
-                }else {
-                    mv.visitVarInsn(ISTORE, local_var_index);
-                }
+                mv.visitVarInsn(ISTORE, local_var_index);
                 break;
             case "Float":
                 mv.visitVarInsn(FSTORE, local_var_index);
@@ -55,11 +79,11 @@ public class AssignmentStatementCodeGenerator {
                 mv.visitVarInsn(ASTORE,local_var_index);
                 break;
             default:
-                throw new Exception("unconsidered case in DragonStatementCodeGenerator. type was :"+expressionInstanceType);
+                throw new Exception("unconsidered case in AssignmentStatementCodeGenerator (visitAssignVarIntoVar). type was :"+expressionInstanceType);
         }
     }
 
-    private void visitAssignVarIntoVar(
+    private static void visitAssignVarIntoArray(
             ClassWriter cw,
             ClassNode classNode,
             AssignmentStatementNode assignmentStatementNode,
@@ -68,22 +92,43 @@ public class AssignmentStatementCodeGenerator {
             LocalVarSymbolTable varTable,
             SubroutineSymbolTable subTable,
             boolean debug
-    ){
-        //x=3;y=4;
-        //x=y;
-
-        //both values are primitive
-
-    }
-
-    private void visitAssignReferenceIntoReference(){
-        //arr1=arr2
-        aoeu
-    }
-
-    private void visitAssignVarIntoArray(){
+    )throws Exception{
         //arr=[1,2];
         //arr[0]=4;
-        aoeu
+        int local_var_index = varTable.getIndexOfVariable(assignmentStatementNode.variableNode.name);
+
+        //push the array reference
+        mv.visitIntInsn(ALOAD,local_var_index);
+
+        //push the index
+        ExpressionCodeGenerator
+                .visitExpression(
+                        cw,mv,classNode,methodNode,
+                        assignmentStatementNode.variableNode.indexOptional.get(),
+                        varTable,subTable,debug
+                );
+
+        //evaluate the expression and store the result in the local variable
+        ExpressionCodeGenerator
+                .visitExpression(cw, mv, classNode, methodNode, assignmentStatementNode.expressionNode, varTable,subTable,debug);
+
+        String expressionInstanceType= TypeResolver.getTypeExpressionNode(assignmentStatementNode.expressionNode,methodNode,subTable,varTable);
+
+        switch(expressionInstanceType){
+            case "Int":
+            case "Bool":
+                mv.visitIntInsn(IASTORE,local_var_index);
+                break;
+            case "Float":
+                mv.visitIntInsn(FASTORE,local_var_index);
+                break;
+            case "String":
+            case "[Int]":
+            case "[Float]":
+                mv.visitVarInsn(ASTORE,local_var_index);
+                break;
+            default:
+                throw new Exception("unconsidered case in AssignmentStatementCodeGenerator. type was :"+expressionInstanceType);
+        }
     }
 }
