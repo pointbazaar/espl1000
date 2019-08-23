8dc3cdcae45ef75dca36f2dde35b5977eb600ad1
pointbazaar <alex23667@gmail.com>
2019-08-23 03:03:41 +0200

    remove a duplicate test class

--- src/test/java/org/vanautrui/languages/phase_clean_the_input/CommentRemoverTest.java
+++ src/test/java/org/vanautrui/languages/phase_clean_the_input/CommentRemoverTest.java
@@ -3,7 +3,11 @@ package org.vanautrui.languages.phase_clean_the_input;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CommentRemoverTest {
+
+    CommentRemover remover=new CommentRemover();
 
     @Test
     public void test_not_remove_comments_in_strings()throws Exception{
@@ -35,5 +39,27 @@ public class CommentRemoverTest {
         String src="println(\"arr=[1,2,3,4]; //looks good to me as an array constant\");";
         String res = cr.strip_single_line_comments(src);
         Assert.assertEquals(src+"\n",res);
+    }
+
+    @Test
+    public void test_remove_single_line_comments() {
+        String result = remover.strip_single_line_comments("command1\n//hello comment\ncommand2\n");
+        assertEquals("command1\n\ncommand2\n", result);
+    }
+
+    @Test
+    public void test_remove_multi_line_comments() throws Exception {
+        String result = remover.strip_multi_line_comments("command1\n/*\ncommand2\ncommand3\n*/command4\n");
+        assertEquals("command1\ncommand4\n", result);
+    }
+
+    @Test
+    public void test_do_remove_nested_multiline_comments() throws Exception {
+        String pseudo_source = "" +
+                "command1\n" +
+                "/*/**/*/\n" +
+                "command4\n";
+        String result = remover.strip_multi_line_comments(pseudo_source);
+        assertEquals("command1\n\ncommand4\n", result);
     }
 }
