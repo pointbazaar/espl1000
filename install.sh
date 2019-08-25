7d69d0576ffe08b16bc357d5b5ce3c74ce40c352
pointbazaar <alex23667@gmail.com>
2019-08-25 20:25:00 +0200

    make install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#OPTIONAL: 
+#read https://medium.com/devnetwork/how-to-create-your-own-custom-terminal-commands-c5008782a78e
+
+#STEP 1: clone this repo to your home directory
+
+#STEP 2:
+echo "source ~/dragon/.dragon.sh" >> ~/.bashrc
+
