a72645ff9382f225f3e574bca72cd126de564f37
pointbazaar <alex23667@gmail.com>
2019-09-01 03:30:16 +0200

    work on making the VMInstr instances

--- vm-compiler/VMInstr.cpp
+++ vm-compiler/VMInstr.cpp
@@ -11,6 +11,24 @@ using namespace std;
 
 vector<VMInstr> VMInstr::make_vm_instrs(vector<string> vmcodes){
 		vector<VMInstr> res;
-		//TODO
+		for(int i=0;i<vmcodes.size();i++){
+				string s = vmcodes.at(i);
+				VMInstr instr;
+				//split it. as of now we have 
+				//a maximum of 3 seperate words
+				int x=s.find(" ");
+				if(x!=string::npos){
+					instr.cmd=s.substr(0,x);
+				}
+				string s2=s.substr(x,s.length());
+				int x2=s2.find(" ");
+				if(x2!=string::npos){
+					instr.arg1=s2.substr(0,x2);
+					instr.arg2=s2.substr(x2,s2.length());
+				}else{
+					instr.arg2=s2;
+				}
+				res.push_back(instr);
+		}
 		return res;
 }
