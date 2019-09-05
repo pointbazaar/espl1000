042715d1698486229f1a7357ccf074e773614896
pointbazaar <alex23667@gmail.com>
2019-09-05 21:12:00 +0200

    print some char

--- assembly-experiments/mem-access/main.asm
+++ assembly-experiments/mem-access/main.asm
@@ -2,7 +2,23 @@ section .text
 	global _start
 
 _start:
+	;try some direct memory access
+	 ;mov eax,4
+         ;mov ebx,1
+	mov eax,'x'
+	push eax
 
+         ;print the char on stack
+         pop ecx ; //pop digit into ecx
+         add ecx,0x30 ; //add ascii offset
+         push ecx ;  //push it back on stack
+         mov ecx,esp ;
+	mov eax,4 ;sys_write
+	mov ebx,1 ;stdout
+         ;val length
+         mov edx,1;
+         int 0x80;
+	
 	;exit
 	mov eax,1
 	int 0x80
