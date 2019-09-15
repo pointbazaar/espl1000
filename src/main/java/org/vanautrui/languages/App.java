902ac31547b67ea437e1301e856ae4fc5ffd9f49
pointbazaar <alex23667@gmail.com>
2019-09-15 21:44:34 +0200

    remove dead code

--- src/main/java/org/vanautrui/languages/App.java
+++ src/main/java/org/vanautrui/languages/App.java
@@ -1,16 +1,8 @@
 package org.vanautrui.languages;
 
-import org.objectweb.asm.AnnotationVisitor;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.FieldVisitor;
-import org.objectweb.asm.MethodVisitor;
 import org.vanautrui.languages.commandline.dragonc;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
-
-import static org.objectweb.asm.Opcodes.*;
 
 public class App {
 
@@ -41,67 +33,4 @@ public class App {
         }
     }
 
-    private static void try_generate_some_bytecode(){
-
-        //https://stackoverflow.com/questions/5346908/generating-a-hello-world-class-with-the-java-asm-library
-
-        //IMPORTANT: there are intellij plugins for viewing bytecode and such.
-
-        //copied from https://stackoverflow.com/questions/5346908/generating-a-hello-world-class-with-the-java-asm-library
-
-        ClassWriter cw = new ClassWriter(0);
-        FieldVisitor fv;
-        MethodVisitor mv;
-        AnnotationVisitor av0;
-
-        cw.visit(49,
-                ACC_PUBLIC + ACC_SUPER,
-                "Hello",
-                null,
-                "java/lang/Object",
-                null);
-
-        cw.visitSource("Hello.java", null);
-
-        {
-            mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
-            mv.visitVarInsn(ALOAD, 0);
-            mv.visitMethodInsn(INVOKESPECIAL,
-                    "java/lang/Object",
-                    "<init>",
-                    "()V");
-            mv.visitInsn(RETURN);
-            mv.visitMaxs(1, 1);
-            mv.visitEnd();
-        }
-        {
-            mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC,
-                    "main",
-                    "([Ljava/lang/String;)V",
-                    null,
-                    null);
-            mv.visitFieldInsn(GETSTATIC,
-                    "java/lang/System",
-                    "out",
-                    "Ljava/io/PrintStream;");
-            mv.visitLdcInsn("hello");
-            mv.visitMethodInsn(INVOKEVIRTUAL,
-                    "java/io/PrintStream",
-                    "println",
-                    "(Ljava/lang/String;)V");
-            mv.visitInsn(RETURN);
-            mv.visitMaxs(2, 1);
-            mv.visitEnd();
-        }
-        cw.visitEnd();
-
-        byte[] mybytecode = cw.toByteArray();
-
-        try {
-            Files.write(Paths.get("Hello.class"), mybytecode);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
 }
