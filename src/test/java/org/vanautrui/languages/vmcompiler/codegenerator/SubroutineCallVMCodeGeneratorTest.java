5e3c7518b31ce0a059005be32c909a608d7f3eb1
pointbazaar <alex23667@gmail.com>
2019-09-17 01:28:21 +0200

    add a more rigorous test for putchar

--- src/test/java/org/vanautrui/languages/vmcompiler/codegenerator/SubroutineCallVMCodeGeneratorTest.java
+++ src/test/java/org/vanautrui/languages/vmcompiler/codegenerator/SubroutineCallVMCodeGeneratorTest.java
@@ -30,4 +30,37 @@ public class SubroutineCallVMCodeGeneratorTest {
     assertEquals(0,pr.exitValue());
     assertEquals("c", IOUtils.toString(pr.getInputStream()));
   }
+
+  @Test
+  public void test_compile_putchar_multile()throws Exception{
+    final int exit_code=234;
+    DracoVMCodeWriter a=new DracoVMCodeWriter();
+
+    a.subroutine("main",0);
+
+    //push the exit code to use later
+    a.iconst(exit_code);
+
+    a.cconst('a');
+    a.call("putchar");
+    a.pop();
+
+    a.cconst('b');
+    a.call("putchar");
+    a.pop();
+
+    a.cconst('c');
+    a.call("putchar");
+    a.pop();
+
+
+    a.exit();
+
+    List<String> vmcodes=a.getDracoVMCodeInstructions();
+
+    Process pr = CodeGeneratorTestUtils.compile_and_run_vm_codes_for_testing(vmcodes, "putchartesting");
+
+    assertEquals(exit_code,pr.exitValue());
+    assertEquals("abc", IOUtils.toString(pr.getInputStream()));
+  }
 }
