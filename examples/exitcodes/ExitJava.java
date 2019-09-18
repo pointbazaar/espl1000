655981bac6ee9a31a6c405d78b4f168dba455c85
pointbazaar <alex23667@gmail.com>
2019-09-18 19:39:49 +0200

    minor changes

--- examples/exitcodes/ExitJava.java
+++
@@ -1,5 +0,0 @@
-public class ExitJava{
-	public static void main(String[] args){
-		System.exit(1);
-	}
-}
--- src/main/java/org/vanautrui/languages/vmcompiler/model/Register.java
+++ src/main/java/org/vanautrui/languages/vmcompiler/model/Register.java
@@ -9,12 +9,14 @@ public class Register {
         this.name=name;
     }
 
+    //general purpose registers
     public static final Register eax=new Register("eax");
     public static final Register ebx=new Register("ebx");
     public static final Register ecx=new Register("ecx");
     public static final Register edx=new Register("edx");
 
     public static final Register esi=new Register("esi");
+    public static final Register edi=new Register("edi");
 
     //stack pointer
     public static final Register esp=new Register("esp");
@@ -22,7 +24,7 @@ public class Register {
     //stack base pointer
     public static final Register ebp=new Register("ebp");
 
-    public static final Register edi=new Register("edi");
+
 
     @Override
     public String toString(){
