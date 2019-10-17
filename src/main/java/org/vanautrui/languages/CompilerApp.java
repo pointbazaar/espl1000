94c4e034b07c9d1e761765e5be15007959d82e11
pointbazaar <alex23667@gmail.com>
2019-10-17 16:38:31 +0200

    delete unused classes and reduce class count

--- src/main/java/org/vanautrui/languages/CompilerApp.java
+++ src/main/java/org/vanautrui/languages/CompilerApp.java
@@ -1,8 +1,12 @@
 package org.vanautrui.languages;
 
+import org.fusesource.jansi.Ansi;
 import org.vanautrui.languages.commandline.dragonc;
 
+import java.io.PrintStream;
 import java.util.Arrays;
+
+import static org.fusesource.jansi.Ansi.ansi;
 
 public class CompilerApp {
 
@@ -28,5 +32,31 @@ public class CompilerApp {
             System.err.println("-help     for information about command line arguments");
             System.exit(1);
         }
+    }
+
+    public static String b(String s){
+        return ansi().fg(Ansi.Color.BLUE).a(s).reset().toString();
+    }
+
+    public static String gererateErrorString(String s){
+        return ansi().fg(Ansi.Color.RED).a(s).reset().toString();
+    }
+
+    public static void println(String s,Ansi.Color color, PrintStream out){
+        print(s,color,out);
+        print("\n",color,out);
+    }
+
+    public static void print(String s, Ansi.Color color,PrintStream out){
+        out.print(ansi().fg(color).a(s).reset());
+    }
+
+    public static void println(String s , Ansi.Color color){
+        print(s,color);
+        print("\n",color);
+    }
+
+    public static void print(String s , Ansi.Color color){
+        print(s,color,System.out);
     }
 }
