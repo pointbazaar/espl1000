91e99df80958ad56678bf3ade7b4f1d88cef309e
pointbazaar <alex23667@gmail.com>
2019-10-20 17:40:36 +0200

    implement the '#' wildcard type in typechecking

--- src/main/java/org/vanautrui/languages/compiler/typechecking/AssignmentStatementTypeChecker.java
+++ src/main/java/org/vanautrui/languages/compiler/typechecking/AssignmentStatementTypeChecker.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.vanautrui.languages.compiler.typechecking.ExpressionNodeTypeChecker.typeCheckExpressionNode;
 import static org.vanautrui.languages.compiler.typechecking.VariableNodeTypeChecker.typeCheckVariableNode;
 
-public class AssignmentStatementTypeChecker {
+public final class AssignmentStatementTypeChecker {
 
   static void typeCheckAssignmentStatementNode(
           List<AST> asts,
@@ -28,7 +28,13 @@ public class AssignmentStatementTypeChecker {
   ) throws Exception{
     final var leftSideType = TypeResolver.getTypeVariableNode(assignmentStatementNode.variableNode,methodNode,subTable,varTable,structsTable);
     final var rightSideType = TypeResolver.getTypeExpressionNode(assignmentStatementNode.expressionNode,methodNode,subTable,varTable,structsTable);
-    if(!leftSideType.getTypeName().equals(rightSideType.getTypeName())){
+
+    final boolean types_equal = leftSideType.getTypeName().equals(rightSideType.getTypeName());
+    final boolean one_is_wildcard = leftSideType.getTypeName().equals("#") || rightSideType.getTypeName().equals("#");
+
+    if(
+            !types_equal && !one_is_wildcard
+    ){
       throw new Exception(
               "with an assignment, both sides have to have the same type. here, a value of type "+rightSideType.getTypeName()+" was assigned to a value of type "+leftSideType.getTypeName()
                       +" in source: '"+assignmentStatementNode.toSourceCode()+"'"
