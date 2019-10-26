32e7c6d27d88e1be5da295129f1418fcd569903e
pointbazaar <alex23667@gmail.com>
2019-10-26 17:40:16 +0200

    work on assembly experiments

--- assembly-experiments/floats/build.sh
+++
@@ -1,2 +0,0 @@
-nasm -f elf floats.asm
-ld -m elf_i386 -s -o floats floats.o
--- assembly-experiments/floats/floats.asm
+++ assembly-experiments/floats/floats.asm
@@ -1,3 +1,7 @@
+; this program is just me trying out 
+; to work with floating point values in assembly
+; https://stackoverflow.com/questions/42181811/how-to-add-two-numbers-integer-and-a-float-in-nasm
+
 section	.text
    global _start     ;must be declared for linker (ld)
 	
@@ -13,18 +17,54 @@ _start:	            ;tells linker entry point
    ;to see if we can do that
 
 	;movss xmm0, 1.0
-	mov eax,num
-	and eax, 07FFFFFh
+	;mov eax,num
+
+	;mov dword eax, __float32__(1.0)
+	;and eax, 07FFFFFh
 	; eax should contain now the 22 bit mantissa
-	; print out eax
-	; push eax on stack,
-	; call printi 
+
+	;DEBUG: TODO: remove this
+	mov eax,13
+
+	; pick out the last digit by using %10
+	; https://stackoverflow.com/questions/8021772/assembly-language-how-to-do-modulo
+	
+	mov ebx,10 ; divisor is 10
+	div ebx  ; divide eax by ebx=10
+	; edx now contains the remainder
+	mov eax,edx
+
+	add eax, 0x30	; convert digit to ascii
+	push 49 ; push ascii '1'
+	call printchar_on_stack ;print the char
+	pop eax
+
+	mov	eax,1       ;system call number (sys_exit)
+	int	0x80        ;call kernel
+
+
+printchar_on_stack:
+	; this subroutine is to print one char, which is on the stack
+	; the stack should not be changed by this subroutine
+
+	mov ecx, esp
+	add ecx, 4 		
+	; as esp points to the next location where the next dword would
+	; be placed on the stack, and in linux the stack grows toward low memory
 
 	
-   mov	eax,1       ;system call number (sys_exit)
-   int	0x80        ;call kernel
+	;put a pointer to our string in ecx
+	;because we are printing the char at top of stack, esp will suffice
+
+	mov eax,4 ;syscall number: sys_write
+	mov ebx,1 ;write to stdout
+	mov edx,1 ;length to be printed
+	int 0x80  ;call kernel		
+	
+	ret
+
 
 section	.data
-num REAL4 -1.5
-msg db 'Hello, world!', 0xa  ;string to be printed
-len equ $ - msg     ;length of the string
+	
+	msg db 'Hello, world!', 0xa  ;string to be printed
+	len equ $ - msg     ;length of the string
