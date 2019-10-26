1c7cac9b4dcb2ad63903c1aee40b388ddc2011d7
pointbazaar <alex23667@gmail.com>
2019-10-26 19:17:59 +0200

    figure out how to obtain the sign of a floating point value

--- assembly-experiments/floats/floats.asm
+++ assembly-experiments/floats/floats.asm
@@ -1,6 +1,8 @@
 ; this program is just me trying out 
 ; to work with floating point values in assembly
 ; https://stackoverflow.com/questions/42181811/how-to-add-two-numbers-integer-and-a-float-in-nasm
+; https://steve.hollasch.net/cgindex/coding/ieeefloat.html
+
 
 section	.text
    global _start     ;must be declared for linker (ld)
@@ -19,26 +21,40 @@ _start:	            ;tells linker entry point
 	;movss xmm0, 1.0
 	;mov eax,num
 
-	;mov dword eax, __float32__(1.0)
-	;and eax, 07FFFFFh
+	mov dword eax, __float32__(1.1875)
+	;https://www.intel.com/content/www/us/en/programmable/documentation/eis1410764818924.html#eis1410767329165
+
+	; to obtain the mantissa
+	;and eax, 0xb 0111 1111 1111 1111 1111 1111; bitmask the mantissa
+	;and eax, 0b011111111111111111111111; bitmask the mantissa
 	; eax should contain now the 22 bit mantissa
 
+	; obtain the exponent, it is 1 for negative numbers, 0 for positives
+	;and eax, 0b1000 0000 0000 0000 0000 0000 0000 0000
+	and eax, 0b10000000000000000000000000000000
+	shr eax, 31 
+
 	;DEBUG: TODO: remove this
-	mov eax,13
+	;mov eax,13
+	
 
 	; pick out the last digit by using %10
 	; https://stackoverflow.com/questions/8021772/assembly-language-how-to-do-modulo
-	
-	mov ebx,10 ; divisor is 10
-	div ebx  ; divide eax by ebx=10
-	; edx now contains the remainder
-	mov eax,edx
 
-	add eax, 0x30	; convert digit to ascii
-	push 49 ; push ascii '1'
-	mov ecx,esp ;for printchar_on_stack
-	call printchar_on_stack ;print the char
-	pop eax
+	
+		xor edx,edx
+		mov ebx,10 ; divisor is 10
+		div ebx  ; divide eax by ebx=10
+		; edx now contains the remainder
+		mov eax,edx
+	
+		add eax, 0x30	; convert digit to ascii 
+		push eax ;push our ascii value on stack
+		;push 49 ; push ascii '1'
+		
+		mov ecx,esp ;for printchar_on_stack
+		call printchar_on_stack ;print the char
+		pop eax
 
 	mov	eax,1       ;system call number (sys_exit)
 	int	0x80        ;call kernel
