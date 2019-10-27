9b815bdc8437bc2de21ae16187f166e20ab88bea
pointbazaar <alex23667@gmail.com>
2019-10-27 16:51:50 +0100

    update dependencies and remove dependency to JANSI, as we do not need pretty printing, and it would only increase .jar size and decrease speed.

--- src/main/java/org/vanautrui/languages/CompilerApp.java
+++ src/main/java/org/vanautrui/languages/CompilerApp.java
@@ -1,12 +1,9 @@
 package org.vanautrui.languages;
 
-import org.fusesource.jansi.Ansi;
 import org.vanautrui.languages.commandline.dragonc;
 
 import java.io.PrintStream;
 import java.util.Arrays;
-
-import static org.fusesource.jansi.Ansi.ansi;
 
 public class CompilerApp {
 
@@ -35,28 +32,24 @@ public class CompilerApp {
     }
 
     public static String b(String s){
-        return ansi().fg(Ansi.Color.BLUE).a(s).reset().toString();
+        return s;
     }
 
-    public static String gererateErrorString(String s){
-        return ansi().fg(Ansi.Color.RED).a(s).reset().toString();
+    public static void println(String s, PrintStream out){
+        print(s,out);
+        print("\n",out);
     }
 
-    public static void println(String s,Ansi.Color color, PrintStream out){
-        print(s,color,out);
-        print("\n",color,out);
+    public static void print(String s, PrintStream out){
+        out.print(s);
     }
 
-    public static void print(String s, Ansi.Color color,PrintStream out){
-        out.print(ansi().fg(color).a(s).reset());
+    public static void println(String s){
+        print(s);
+        print("\n");
     }
 
-    public static void println(String s , Ansi.Color color){
-        print(s,color);
-        print("\n",color);
-    }
-
-    public static void print(String s , Ansi.Color color){
-        print(s,color,System.out);
+    public static void print(String s ){
+        print(s,System.out);
     }
 }
--- src/main/java/org/vanautrui/languages/commandline/CompilerPhaseUtils.java
+++ src/main/java/org/vanautrui/languages/commandline/CompilerPhaseUtils.java
@@ -1,64 +1,24 @@
 package org.vanautrui.languages.commandline;
 
-import org.fusesource.jansi.Ansi;
 import org.vanautrui.languages.CompilerApp;
-
-import static org.fusesource.jansi.Ansi.Color.GREEN;
-import static org.fusesource.jansi.Ansi.Color.RED;
-import static org.fusesource.jansi.Ansi.ansi;
 
 public class CompilerPhaseUtils {
     public static void printBeginPhase(String phaseName,boolean printLongForm) throws InterruptedException {
-        //TerminalUtil.print(String.format("%-18s ",phaseName),Ansi.Color.GREEN);
-        Ansi ansi = Ansi.ansi();
-        if(!printLongForm) {
-            ansi.eraseLine(Ansi.Erase.BACKWARD);
-        }
 
-        ansi.fg(GREEN);
-        ansi.a(phaseName);
-
-        ansi.reset();
-        System.out.print(ansi);
-        System.out.flush();
-
-        //rip this out, this is slowing down the compiler
-        //Thread.sleep(100);
+        System.out.println(phaseName);
     }
 
-    public static void printEndPhase(boolean success, boolean printLongForm) throws Exception{
-        Ansi ansi1=Ansi.ansi();
-        if(!printLongForm) {
-            ansi1.cursorToColumn(0);
-        }
+    public static void printEndPhase(boolean success, boolean debug) {
 
-        ansi1.cursorToColumn(18);
-        if(success) {
-            ansi1.fg(GREEN);
-            ansi1.a("✓");
-        }else{
-            ansi1.fg(RED);
-            ansi1.a("⚠");
+        if(debug) {
+            System.out.println((success)?"✓":"⚠");
         }
-        ansi1.cursorToColumn(0);
-        if(printLongForm){
-            ansi1.newline();
-        }
-        if(!printLongForm) {
-            ansi1.eraseLine(Ansi.Erase.FORWARD);
-        }
-        ansi1.reset();
-        System.out.print(ansi1);
-        System.out.flush();
-
-        //rip this out, this is slowing down the compiler
-        //Thread.sleep(100);
     }
     public static void printBuildConclusion(boolean success){
         if(success) {
-            System.out.println(ansi().fg(GREEN).bold().a("BUILD SUCCESS").reset());
+            System.out.println("BUILD SUCCESS");
         }else {
-            System.out.println(ansi().fg(RED).bold().a("BUILD FAILURE").reset());
+            System.out.println("BUILD FAILURE");
         }
     }
 
@@ -70,13 +30,13 @@ public class CompilerPhaseUtils {
     public static void printDurationFeedback(long duration /*milliseconds*/){
         String str = duration + " ms";
         if(duration>500) {
-            CompilerApp.println("☠ "+str+" Compilation took too long. This needs to be fixed. Please file an Issue on GitHub.", Ansi.Color.RED);
+            CompilerApp.println("☠ "+str+" Compilation took too long. This needs to be fixed. Please file an Issue on GitHub.");
         }else if(duration>200) {
-            CompilerApp.println("☠ "+str+" we are truly sorry for the delay :(", Ansi.Color.YELLOW);
+            CompilerApp.println("☠ "+str+" we are truly sorry for the delay :(");
         }else if(duration>100){
-            CompilerApp.println("✝ "+str+" sorry it took so long!", Ansi.Color.YELLOW);
+            CompilerApp.println("✝ "+str+" sorry it took so long!");
         }else {
-            CompilerApp.println("☕ " + str, Ansi.Color.GREEN);
+            CompilerApp.println("☕ " + str);
         }
     }
 }
