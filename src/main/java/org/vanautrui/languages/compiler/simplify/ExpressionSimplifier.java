25300255b01d08cd0d9b44bb0e25814d8654af81
pointbazaar <alex23667@gmail.com>
2020-01-03 01:46:30 +0100

    refactor to reduce line count in a single file

---
+++ src/main/java/org/vanautrui/languages/compiler/simplify/ExpressionSimplifier.java
@@ -0,0 +1,179 @@
+package org.vanautrui.languages.compiler.simplify;
+
+import org.vanautrui.languages.compiler.parsing.astnodes.nonterminal.ExpressionNode;
+import org.vanautrui.languages.compiler.parsing.astnodes.nonterminal.OperatorNode;
+import org.vanautrui.languages.compiler.parsing.astnodes.nonterminal.TermNode;
+import org.vanautrui.languages.compiler.parsing.astnodes.terminal.BoolConstNode;
+import org.vanautrui.languages.compiler.parsing.astnodes.terminal.FloatConstNode;
+import org.vanautrui.languages.compiler.parsing.astnodes.terminal.IConstantNode;
+import org.vanautrui.languages.compiler.parsing.astnodes.terminal.IntConstNode;
+
+public final class ExpressionSimplifier {
+
+
+	public static TermNode mayUnwrapExpression(final ExpressionNode expr, final boolean debug)throws Exception{
+		//TODO: use this subroutine where it makes sense
+		if(expr.op.isEmpty()){
+			return expr.term1;
+		}
+		throw new Exception("could not unwrap.");
+	}
+
+	public static ExpressionNode simplifyExpressionNode(final ExpressionNode expr,final boolean debug){
+
+		if(expr.op.isEmpty()){
+			//nothing to do, as there is just 2 expression
+			return new ExpressionNode(simplifyTermNode(expr.term1,debug));
+		}
+
+		//op and term2 should be there
+		final TermNode term1 = expr.term1;
+		final OperatorNode op = expr.op.get();
+		final TermNode term2 = expr.term2.get();
+
+		return simplifyExpressionInner(term1,op,term2,debug);
+	}
+
+	private static TermNode simplifyTermNode(TermNode term1,final boolean debug) {
+
+		if(term1.termNode instanceof ExpressionNode){
+			return new TermNode(simplifyExpressionNode((ExpressionNode)term1.termNode,debug));
+		}
+		return term1;
+	}
+
+	public static ExpressionNode simplifyExpressionInner(
+			final TermNode term1,
+			final OperatorNode op,
+			final TermNode term2,
+			final boolean debug
+	){
+		//this is in the case we have expression with 2 terms
+		//TODO: there should be a binary tree where each of the 2 leaves is guaranteed to exist
+		//TODO: this would make these routines much simpler
+
+		//try to simplify terms in case there is a subexpression among them that can be simplified.
+		TermNode nTerm1 = simplifyTermNode(term1,debug);
+		TermNode nTerm2 = simplifyTermNode(term2,debug);
+
+		//TODO: maybe lift a term from expression in term. this could be the cause for the failing test
+		if(nTerm1.termNode instanceof ExpressionNode){
+			try {
+				nTerm1 = mayUnwrapExpression((ExpressionNode)nTerm1.termNode,debug);
+			} catch (Exception e) {
+				//pass
+			}
+		}
+
+		if(nTerm2.termNode instanceof ExpressionNode){
+			try {
+				nTerm2 = mayUnwrapExpression((ExpressionNode)nTerm2.termNode,debug);
+			} catch (Exception e) {
+				//pass
+			}
+		}
+
+		//if we then have 2 constant expressions, we can simplify it.
+		if(nTerm1.termNode instanceof IConstantNode && nTerm2.termNode instanceof IConstantNode){
+			return new ExpressionNode(evaluateConstantExpression(
+					(IConstantNode)nTerm1.termNode,
+					op,
+					(IConstantNode)nTerm2.termNode,
+					debug
+			));
+		}else{
+			return new ExpressionNode(nTerm1,op,nTerm2);
+		}
+	}
+
+	private static TermNode evaluateConstantExpression(IConstantNode term1, OperatorNode op, IConstantNode term2, boolean debug) {
+		//typechecking has already happened, so
+		//we do not need to confirm the types here again.
+
+		if(term1 instanceof IntConstNode
+		   && term2 instanceof IntConstNode
+		){
+			final IntConstNode i1 = (IntConstNode)term1;
+			final IntConstNode i2 = (IntConstNode)term2;
+			switch (op.operator){
+				//arithmetic
+				case "+":
+					return new TermNode(new IntConstNode(i1.number + i2.number));
+				case "-":
+					return new TermNode(new IntConstNode(i1.number - i2.number));
+				case "*":
+					return new TermNode(new IntConstNode(i1.number * i2.number));
+				case "/":
+					return new TermNode(new IntConstNode(i1.number / i2.number));
+				case "%":
+					return new TermNode(new IntConstNode(i1.number % i2.number));
+
+				//comparisons
+				case ">":
+					return new TermNode(new BoolConstNode(i1.number > i2.number));
+				case ">=":
+					return new TermNode(new BoolConstNode(i1.number >= i2.number));
+				case "<":
+					return new TermNode(new BoolConstNode(i1.number < i2.number));
+				case "<=":
+					return new TermNode(new BoolConstNode(i1.number <= i2.number));
+				case "!=":
+					return new TermNode(new BoolConstNode(i1.number != i2.number));
+				case "==":
+					return new TermNode(new BoolConstNode(i1.number == i2.number));
+			}
+
+		}else if(
+				term1 instanceof FloatConstNode
+				&& term2 instanceof FloatConstNode
+		){
+			final FloatConstNode i1 = (FloatConstNode)term1;
+			final FloatConstNode i2 = (FloatConstNode)term2;
+			switch (op.operator){
+				//arithmetic
+				case "+":
+					return new TermNode(new FloatConstNode(i1.floatValue + i2.floatValue));
+				case "-":
+					return new TermNode(new FloatConstNode(i1.floatValue - i2.floatValue));
+				case "*":
+					return new TermNode(new FloatConstNode(i1.floatValue * i2.floatValue));
+				case "/":
+					return new TermNode(new FloatConstNode(i1.floatValue / i2.floatValue));
+				case "%":
+					return new TermNode(new FloatConstNode(i1.floatValue % i2.floatValue));
+
+				//comparisons
+				case ">":
+					return new TermNode(new BoolConstNode(i1.floatValue > i2.floatValue));
+				case ">=":
+					return new TermNode(new BoolConstNode(i1.floatValue >= i2.floatValue));
+				case "<":
+					return new TermNode(new BoolConstNode(i1.floatValue < i2.floatValue));
+				case "<=":
+					return new TermNode(new BoolConstNode(i1.floatValue <= i2.floatValue));
+				case "!=":
+					return new TermNode(new BoolConstNode(i1.floatValue != i2.floatValue));
+				case "==":
+					return new TermNode(new BoolConstNode(i1.floatValue == i2.floatValue));
+			}
+		}else if(
+				term1 instanceof BoolConstNode
+				&& term2 instanceof BoolConstNode
+		){
+			final BoolConstNode i1 = (BoolConstNode)term1;
+			final BoolConstNode i2 = (BoolConstNode)term2;
+			switch (op.operator){
+				case "&&":
+					return new TermNode(new BoolConstNode(i1.boolValue && i2.boolValue));
+				case "||":
+					return new TermNode(new BoolConstNode(i1.boolValue || i2.boolValue));
+				case "!=":
+					return new TermNode(new BoolConstNode(i1.boolValue != i2.boolValue));
+				case "==":
+					return new TermNode(new BoolConstNode(i1.boolValue == i2.boolValue));
+			}
+		}
+
+		throw new RuntimeException("fatality");
+	}
+}
