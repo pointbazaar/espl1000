79d7559e17b67e6680d978770f9665bdb1949ef6
pointbazaar <alex23667@gmail.com>
2020-02-07 23:30:22 +0100

    use a makefile and parallel build.

--- dragon-parser/buildandtest.sh
+++ dragon-parser/buildandtest.sh
@@ -2,16 +2,7 @@
 
 # do not use .hpp's in g++ call
 
-g++ -O0 -march=native -g --std=c++17 -o dragon-parser \
-src/main/commandline/Parser.cpp \
-src/main/commandline/ASTWriter.cpp \
-src/main/commandline/BaseToken.cpp \
-src/main/commandline/TokenList.cpp \
-src/main/commandline/TokenReader.cpp \
-src/main/parsing/*.cpp \
-src/main/parsing/statements/*cpp \
-src/main/parsing/typenodes/*.cpp \
-
+make -j8
 
 #copy the executable to /usr/bin
 cp dragon-parser /usr/bin/
