bf2194f53e9accd6f8ee9e5018a177daf17c5461
pointbazaar <alex23667@gmail.com>
2020-03-15 23:12:44 +0100

    add syntax highlighting for dragon, for sublime-text. it still needs to be improved

---
+++ syntax-tools/sublime-text/install.sh
@@ -0,0 +1,6 @@
+mkdir ~/.config/sublime-text-3/Packages/User
+
+zip dragon.zip dragon.sublime-syntax
+mv dragon.zip dragon.sublime-package
+
+cp dragon.sublime-package ~/.config/sublime-text-3/Packages/User
