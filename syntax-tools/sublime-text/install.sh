2752c001fdf94e0bec385f934433820c17afff97
pointbazaar <alex23667@gmail.com>
2020-03-15 23:43:20 +0100

    improvements for the syntax highlighting

--- syntax-tools/sublime-text/install.sh
+++ syntax-tools/sublime-text/install.sh
@@ -1,6 +1,6 @@
-mkdir ~/.config/sublime-text-3/Packages/User
+#mkdir ~/.config/sublime-text-3/Packages/User
 
-zip dragon.zip dragon.sublime-syntax
-mv dragon.zip dragon.sublime-package
+#zip dragon.zip dragon.sublime-syntax
+#mv dragon.zip dragon.sublime-package
 
-cp dragon.sublime-package ~/.config/sublime-text-3/Packages/User
+cp dragon.sublime-syntax ~/.config/sublime-text-3/Packages/User
