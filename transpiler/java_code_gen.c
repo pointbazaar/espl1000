1291f282eb082d4853c9ab4c1efe759061f1d62b
pointbazaar <alex23667@gmail.com>
2020-07-11 01:12:19 +0200

    work on the codegenerator

--- transpiler/java_code_gen.c
+++ transpiler/java_code_gen.c
@@ -7,8 +7,23 @@ int gen_java_code(struct AST_Whole_Program* ast, char* fname_out){
 
 	printf("writing to: %s\n", fname_out);
 	//TODO
-	//FILE* fout = fopen(fname_out, "w");
+	FILE* fout = fopen(fname_out, "w");
 
-	//fclose(fout);
+	const int ns_count = ast->count_namespaces;
+
+	//for each namespace, we need a new java file
+	for(int i=0;i < ns_count; i++){
+			gen_java_namespace(ast->namespaces[i], fout);
+	}
+
+	fclose(fout);
 	return 0;
+}
+
+
+void gen_java_namespace(struct Namespace* ns, FILE* file){
+	
+	fprintf(file, "public class %s {\n", ns->name);
+	
+	fprintf(file, "}\n");
 }
--- transpiler/java_code_gen.h
+++ transpiler/java_code_gen.h
@@ -5,4 +5,6 @@
 
 int gen_java_code(struct AST_Whole_Program* ast, char* fname_out);
 
+void gen_java_namespace(struct Namespace* ns, FILE* file);
+
 #endif
