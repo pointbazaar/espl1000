d6ffad9d2105f223ddd75ef8d5a8093f98d86d2a
pointbazaar <alex23667@gmail.com>
2020-08-10 02:49:16 +0200

    work on the skeleton for the integration tests for smalldragon/transpiler

--- transpiler/test/test.c
+++ transpiler/test/test.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <inttypes.h>
 
 #include "test.h"
 
+bool test_statuscode(bool debug);
+
 void test_all(bool debug){
 	
-	//TODO
-	printf("running tests: TODO\n");
+	printf("running tests for smalldragon/transpiler:\n");
+	uint16_t testsRun    = 0;
+	uint16_t testsPassed = 0;
+	
+	const uint16_t num_tests_max = 1;
+	
+	bool (*tests[num_tests_max]) (bool debug);
+	
+	tests[0] = test_statuscode;
+	
+	for(int i=0;i < num_tests_max; i++){
+		testsPassed += tests[i](debug);
+		testsRun++;
+		if(testsPassed < testsRun){
+			printf("last test did not pass!\n");
+			if(!debug){
+				printf("Isolating the failing Test:\n");
+				printf("------------------------------\n");
+				
+				tests[i](true);
+			}
+			break;
+		}
+	}
+	
+	printf("passed %d of %d(this run) of %d(total test suite) \n",testsPassed,testsRun, num_tests_max);
+}
+
+
+// TESTS START
+
+bool test_statuscode(bool debug){
+	
+	return true;
 }
