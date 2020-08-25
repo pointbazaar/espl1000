8fb3049cabaf1d44634b652e46130920a189925e
pointbazaar <alex23667@gmail.com>
2020-08-25 18:44:28 +0200

    clean up the examples

--- examples/build.sh
+++ examples/build.sh
@@ -1,9 +1,12 @@
 sd booleans/Booleans.dg
+
 sd chars/Chars.dg
+
 sd cmd-args/CmdArgs.dg
+
 sd floats/Floats.dg
 
-sd for_atmel/loop_add_1/Main.dg
+sd for_atmel/LoopAdd1.dg
 sd for_atmel/Main.dg
 
 sd functional/Filter.dg
@@ -16,25 +19,19 @@ sd input/Input.dg
 sd local_variables/Locals.dg
 sd local_variables/Locals2.dg
 
-sd loops/Loop.dg
-sd loops/WhileExample.dg
+sd loops/EmptyLoop.dg
 sd loops/WhileLoop.dg
 
-sd mathematics/Basic.dg
 sd mathematics/Faculty.dg
 sd mathematics/Fibonacci.dg
 
 sd methodCalls/MethodCall.dg
-sd methodCalls/MethodCall3.dg
 
 sd multiline_strings/MultiLineStrings.dg
 
-sd other/Other1.dg
-sd other/Other2.dg
 sd other/Simple.dg
 
 sd structs/MyStructs.dg
 
-sd use/Use.dg
 
 
