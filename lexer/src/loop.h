d36c61ef604ae1892a4775bd8c0b700e8925de3c
pointbazaar <alex23667@gmail.com>
2020-08-26 20:12:59 +0200

    move includes into header guard

--- lexer/src/loop.h
+++ lexer/src/loop.h
@@ -1,11 +1,9 @@
-//standard headers
-#include <stdbool.h>
-
-//user headers
-#include "tokens.h"
-
 #ifndef LOOP_
 #define LOOP_
+
+#include <stdbool.h>
+
+#include "tokens.h"
 
 struct Token** lex(char* clean_source, char* tkn_filename);
 struct Token** lex_main(char* tkn_filename, char* input_filename, long input_file_length, bool free_tokens);
