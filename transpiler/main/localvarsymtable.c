b0321613d3b27a6c33cad26893733c71af86bbb6
pointbazaar <alex23667@gmail.com>
2020-08-29 02:03:32 +0200

    implement some methods related to the local variable symbol table

--- transpiler/main/localvarsymtable.c
+++ transpiler/main/localvarsymtable.c
@@ -1,13 +1,18 @@
-
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "localvarsymtable.h"
 
 struct LocalVarSymTable* makeLocalVarSymTable(struct Method* subr){
 	
-	//TODO: implement
+	struct LocalVarSymTable* lvst = malloc(sizeof(struct LocalVarSymTable));
 	
-	return NULL;
+	lvst->count = 0;
+	lvst->capacity = 10;
+	lvst->lines = malloc(sizeof(struct LVSTLine*)*lvst->capacity);
+	
+	return lvst;
 }
 
 void freeLVSTLine(struct LVSTLine* l){
@@ -18,4 +23,54 @@ void freeLVSTLine(struct LVSTLine* l){
 	//context of the local variable symbol table
 	
 	free(l);
+}
+
+void lvst_add(struct LocalVarSymTable* lvst, struct LVSTLine* line){
+	
+	//the local var symbol table works as a set
+	//with 'name' as the key
+	
+	for(int i = 0; i < lvst->count; i++){
+		
+		struct LVSTLine* current_line = lvst->lines[i];
+		
+		if(strcmp(current_line->name, line->name) == 0){
+			
+			//this local var is already present
+			return;
+		}
+	}
+	
+	//add the line
+	if(lvst->count >= lvst->capacity){
+		
+		//resize the lvst
+		lvst->capacity *= 2;
+		
+		lvst->lines = 
+			realloc(
+				lvst->lines, 
+				sizeof(struct LVSTLine*) * (lvst->capacity)
+			);
+	}
+	
+	lvst->lines[lvst->count] = line;
+	lvst->count += 1;
+}
+
+struct LVSTLine* lvst_get(struct LocalVarSymTable* lvst, char* name){
+	
+	for(int i = 0; i < lvst->count; i++){
+		
+		struct LVSTLine* line = lvst->lines[i];
+		
+		if(strcmp(line->name, name) == 0){
+			
+			return line;
+		}
+	}
+	
+	printf("Fatal Error: %s not found in localvarsymtable\n", name);
+	exit(1);
+	return NULL;
 }
--- transpiler/main/localvarsymtable.h
+++ transpiler/main/localvarsymtable.h
@@ -10,12 +10,14 @@ struct LocalVarSymTable {
 	//local variable symbol table
 	
 	unsigned int count;
+	size_t capacity;
 	
-	//name, type, struct AssignStmt* firstOccur
+	struct LVSTLine** lines;
 };
 
 struct LVSTLine {
 	//a line in the 'local variable symbol table'
+	//name, type, struct AssignStmt* firstOccur
 	
 	//max 32 chars for a local variable identifier
 	char name[32]; 
@@ -29,10 +31,28 @@ struct LVSTLine {
 	//where this local variable was 
 	//assigned
 	struct AssignStmt* firstOccur;
+	
+	
+	//TODO: we must differentiate if 
+	//it is an argument to the function
+	//or not.
+	//because arguments already have a type declared
+	//(for the c code generation)
+	
+	
+	//is it an argument
+	bool isArg;
 };
 
 void freeLVSTLine(struct LVSTLine* l);
 
 struct LocalVarSymTable* makeLocalVarSymTable(struct Method* subr);
+
+//to add a row to the local variable symbol table
+//the lvst works as a set regarding the 'name' of the local variable
+void lvst_add(struct LocalVarSymTable* lvst, struct LVSTLine* line);
+
+//obtain a reference to the line for that identifier
+struct LVSTLine* lvst_get(struct LocalVarSymTable* lvst, char* name);
 
 #endif
