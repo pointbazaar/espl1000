436b8800fc2eba0d85b4ade92cbcdf63834c5cd0
pointbazaar <alex23667@gmail.com>
2020-09-01 20:03:45 +0200

    work on sd -help

--- transpiler/main/help.c
+++ transpiler/main/help.c
@@ -3,25 +3,26 @@
 
 void sd_print_help(){
 	
-	printf("NAME\n");
-	printf("\tsd - smalldragon transpiler\n\n");
+	printf("NAME:     sd - smalldragon transpiler\n\n");
 	
-	printf("SYNOPSIS\n");
-	printf("\tsd [OPTION] FILE\n\n");
+	printf("SYNOPSIS: sd [OPTION] FILE\n\n");
 	
-	printf("OPTIONS:\n");
+	printf("OPTIONS:\n\n");
 	
-	printf("-help		display help\n");
-	printf("-avr		transpile for AVR MCU's (experimental)\n");
-	printf("-test		run the tests for smalldragon\n");
-	printf("-debug		print debug statements\n");
+	printf(" -help		display help\n");
+	printf(" -avr		transpile for AVR MCU's (experimental)\n");
+	printf(" -test		run the tests for smalldragon\n");
+	printf(" -debug		print debug statements\n");
+	
+	printf("\n");
+	
 	printf("All other options will be forwarded to gcc\n");
 	
 	printf("\n");
 	
 	printf("EXAMPLES\n\n");
 	printf("\tsd -test\n");
-	printf("\tsd -avr Test.dg\n");
+	printf("\tsd Test.dg\n");
 	printf("\tsd -help\n");
 	printf("\n");
 	
