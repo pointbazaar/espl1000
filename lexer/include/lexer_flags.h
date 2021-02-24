b330481e29d1400cf87a789f091f61ae5b6945e0
pointbazaar <alex23667@gmail.com>
2021-02-24 09:37:47 +0100

    restructure directories for lexer

---
+++ lexer/include/lexer_flags.h
@@ -0,0 +1,18 @@
+#ifndef LEXER_FLAGS
+#define LEXER_FLAGS
+
+#include <stdbool.h>
+
+struct LexerFlags{
+	
+	char* filename;
+	
+	bool debug;
+	bool clean;
+	bool test;
+	bool version;
+	bool help;
+};
+
+
+#endif
