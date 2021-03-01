efe915d480dd0e49d8aa64efef57218324f8a3d7
pointbazaar <alex23667@gmail.com>
2021-03-01 18:53:45 +0100

    fix an issue concerning too much nested if statements. found in code review.

--- parser/main/astnodes/var/Variable.c
+++ parser/main/astnodes/var/Variable.c
@@ -22,41 +22,42 @@ struct Variable* makeVariable(struct TokenList* tokens, bool debug) {
 	if(list_size(tokens) < 1){ return NULL; }
 
 	struct Variable* res = malloc(sizeof(struct Variable));
-	res->simpleVar = NULL;
 
+	res->simpleVar = NULL;
 	res->memberAccess = NULL;
 
 	struct TokenList* copy = list_copy(tokens);
 
 	res->simpleVar = makeSimpleVar(copy,debug);
+
 	if(res->simpleVar == NULL){
 		freeTokenListShallow(copy);
 		free(res);
 		return NULL;
 	}
 
-	if (list_size(copy) >= 2) {
-		struct Token* next = list_head(copy);
+	if (list_size(copy) < 2) { goto end; }
+	
+	struct Token* next = list_head(copy);
 
-		if(next->kind == STRUCTMEMBERACCESS){
+	if(next->kind != STRUCTMEMBERACCESS){ goto end; }
 
-			list_expect(copy, STRUCTMEMBERACCESS);
+	list_expect(copy, STRUCTMEMBERACCESS);
 
-			res->memberAccess = makeVariable(copy, debug);
-			if(res->memberAccess == NULL){
+	res->memberAccess = makeVariable(copy, debug);
 
-				printf("Error parsing struct member access!\n");
-				printf("%s\n", list_code(copy, debug));
-				free(res);
-				freeTokenListShallow(copy);
-				exit(1);
-			}
-		}
+	if(res->memberAccess == NULL){
+
+		printf("Error parsing struct member access!\n");
+		printf("%s\n", list_code(copy, debug));
+		free(res);
+		freeTokenListShallow(copy);
+		exit(1);
 	}
 	
-	if(debug){
-		printf("sucess parsing Variable\n");
-	}
+	end:
+
+	if(debug){ printf("sucess parsing Variable\n"); }
 
 	list_set(tokens,copy);
 	freeTokenListShallow(copy);
