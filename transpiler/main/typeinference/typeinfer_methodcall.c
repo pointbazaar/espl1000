e2bbb297e3512b65b35de08caafa258a40e4c435
pointbazaar <alex23667@gmail.com>
2021-08-11 01:46:17 +0200

    delete commented (dead) code

--- transpiler/main/typeinference/typeinfer_methodcall.c
+++ transpiler/main/typeinference/typeinfer_methodcall.c
@@ -3,40 +3,10 @@
 #include "typeinference/typeinfer.h"
 
 #include "tables/symtable/symtable.h"
-#include "tables/sst/sst.h"
 
 struct Type* infer_type_methodcall(struct ST* st, struct Call* m){
-
-	//to also infer it if the subr being called
-	//was a local var or arg to the subroutine
 
     struct Type* type = infer_type_variable(st, m->callable);
 
     return type->basic_type->subr_type->return_type;
-
-    /*
-	if(lvst_contains(st->lvst, m->callable->simple_var->name)){
-		
-		struct LVSTLine* line2 = lvst_get(st->lvst, m->callable->simple_var->name);
-		
-		struct Type* type = line2->type;
-		
-		if(type->basic_type == NULL){
-			//ERROR
-			goto other;
-		}
-		if(type->basic_type->subr_type == NULL){
-			//ERROR
-			goto other;
-		}
-		
-		struct SubrType* stype = type->basic_type->subr_type;
-		
-		return stype->return_type;
-	}
-	
-	other:
-	
-	return sst_get(st->sst, m->callable->simple_var->name)->return_type;
-    */
 }
