619047fe1dc0e2cd86577a8e5384643945195bb3
pointbazaar <alex23667@gmail.com>
2022-06-06 22:03:31 +0200

    fix a bug when compiling return statements

--- compiler/main/gen_tac/gen_tac_retstmt.c
+++ compiler/main/gen_tac/gen_tac_retstmt.c
@@ -8,10 +8,8 @@ void tac_retstmt(struct TACBuffer* buffer, struct RetStmt* r){
     if(r->return_value != NULL){
         tac_expr(buffer, r->return_value);
     }
-
-    struct TAC* t = makeTAC();
-    t->kind = TAC_RETURN;
-    strcpy(t->arg1, buffer->buffer[buffer->count-1]->dest);
+    
+    struct TAC* t = makeTACReturn(buffer->buffer[buffer->count-1]->dest);
 
     tacbuffer_append(buffer, t, true);
 }
