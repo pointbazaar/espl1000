23a0165b94753d3e139caee85e1ca3c45d184a2e
pointbazaar <alex23667@gmail.com>
2022-06-06 22:07:03 +0200

    minor changes

--- compiler/main/gen_tac/gen_tac_retstmt.c
+++ compiler/main/gen_tac/gen_tac_retstmt.c
@@ -8,7 +8,7 @@ void tac_retstmt(struct TACBuffer* buffer, struct RetStmt* r){
     if(r->return_value != NULL){
         tac_expr(buffer, r->return_value);
     }
-    
+
     struct TAC* t = makeTACReturn(buffer->buffer[buffer->count-1]->dest);
 
     tacbuffer_append(buffer, t, true);
