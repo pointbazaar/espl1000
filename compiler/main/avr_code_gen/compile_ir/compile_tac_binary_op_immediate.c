af03b30607b2e93033a0ccc46de58ece9cd99247
pointbazaar <alex23667@gmail.com>
2022-10-19 21:04:28 +0200

    expand testing of compilation of TAC_BINARY_OP_IMMEDIATE

--- compiler/main/avr_code_gen/compile_ir/compile_tac_binary_op_immediate.c
+++ compiler/main/avr_code_gen/compile_ir/compile_tac_binary_op_immediate.c
@@ -5,6 +5,9 @@
 #include "tac/tac.h"
 #include "avr_code_gen/compile_ir/compile_tac.h"
 
+static void case_tac_op_add(FILE* fout, int dest, int32_t immediate);
+static void case_tac_op_sub(FILE* fout, int dest, int32_t immediate);
+
 void compile_tac_binary_op_immediate(struct RAT* rat, struct TAC* tac, FILE* fout){
 
 	if(!rat_has_register(rat, tac->dest)){
@@ -20,19 +23,11 @@ void compile_tac_binary_op_immediate(struct RAT* rat, struct TAC* tac, FILE* fou
     switch(tac->op){
 
         case TAC_OP_ADD:
-            if(immediate == 1){
-                fprintf(fout, "inc r%d\n", dest);
-            }else{
-                printf("fatal error in compile_tac_binary_op_immediate\n");
-                exit(1);
-            }
+            case_tac_op_add(fout, dest, immediate);
             break;
 
         case TAC_OP_SUB:
-            if(immediate == 1)
-                fprintf(fout, "dec r%d\n", dest);
-            else
-                fprintf(fout, "subi r%d, $%d\n", dest, immediate);
+            case_tac_op_sub(fout, dest, immediate);
             break;
 
         case TAC_OP_AND:
@@ -48,4 +43,24 @@ void compile_tac_binary_op_immediate(struct RAT* rat, struct TAC* tac, FILE* fou
             printf("tac->op = %d\n", tac->op);
             exit(1);
     }
+}
+
+static void case_tac_op_add(FILE* fout, int dest, int32_t immediate){
+	
+	if(immediate == 1){
+		fprintf(fout, "inc r%d\n", dest);
+	}else{
+		//here we negate the constant for subi
+		//https://sites.google.com/site/avrasmintro/home/basic-arithmetic
+		fprintf(fout, "subi r%d, $%d\n", dest, -immediate);
+	}
+}
+
+
+static void case_tac_op_sub(FILE* fout, int dest, int32_t immediate){
+	
+	if(immediate == 1)
+		fprintf(fout, "dec r%d\n", dest);
+	else
+		fprintf(fout, "subi r%d, $%d\n", dest, immediate);
 }
--- compiler/test/avr_code_gen/test_avr_code_gen.c
+++ compiler/test/avr_code_gen/test_avr_code_gen.c
@@ -21,6 +21,7 @@ static void test_tac_nop();
 static void test_tac_const_value();
 static void test_tac_store_const_addr();
 static void test_tac_load_const_addr();
+static void test_tac_binary_op_immediate();
 
 static void status_test_codegen(char* msg){
     printf(" - [TEST] avr codegen %s\n", msg);
@@ -35,6 +36,7 @@ void test_suite_avr_code_gen(){
     test_tac_const_value();
     test_tac_store_const_addr();
     test_tac_load_const_addr();
+    test_tac_binary_op_immediate();
     //TODO: add more tests to cover all TAC
 }
 
@@ -253,6 +255,70 @@ static void test_tac_load_const_addr(){
 	}
 	
 	assert(found);
+	
+	vmcu_system_dtor(system);
+}
+
+static void test_tac_binary_op_immediate(){
+
+	status_test_codegen("TAC_BINARY_OP_IMMEDIATE");
+	
+	//we test +,-,|,& operators
+	uint8_t change = 40;
+	
+	int8_t starts[] = {rand()%100,rand()%100,rand()%100,rand()%100};
+	int8_t changes[] = {rand()%change,rand()%change,rand()%change,rand()%change};
+	int8_t expected[] = {
+		starts[0] + changes[0],
+		starts[1] - changes[1],
+		starts[2] | changes[2],
+		starts[3] & changes[3],
+	};
+	
+	char* temps[] = {"t0","t1","t2","t3"};
+	enum TAC_OP ops[] = {TAC_OP_ADD, TAC_OP_SUB, TAC_OP_OR, TAC_OP_AND};
+
+    struct TACBuffer* buffer = tacbuffer_ctor();
+    
+    //add all the operations
+    for(int i=0; i < 4; i++){
+		
+		tacbuffer_append(buffer, makeTACConst(i, starts[i]), false);
+		tacbuffer_append(buffer, makeTACBinOpImmediate(temps[i], ops[i], changes[i]), false);
+	}
+    
+    tacbuffer_append(buffer, makeTACReturn("t0"), false);
+
+    vmcu_system_t* system = prepare_vmcu_system_from_tacbuffer(buffer);
+
+	//keep track of which values have initialized and which changed
+	bool has_init[]    = {false,false,false,false};
+	bool has_changed[] = {false,false,false,false};
+
+    for(int i=0;i < 20; i++){
+		
+        vmcu_system_step(system);
+        
+        for(int k = 0; k < 32; k++){
+			
+			const int8_t value = vmcu_system_read_gpr(system, k);
+			
+			for(int j = 0; j < 4; j++){
+				
+				if(has_init[j] && value == expected[j]) 
+					has_changed[j] = true;
+			
+				if(value == starts[j]) 
+					has_init[j] = true;
+			}
+		}
+	}
+	
+	for(int j = 1; j < 2; j++){
+		
+		assert(has_init[j]);
+		assert(has_changed[j]);
+	}
 	
 	vmcu_system_dtor(system);
 }
