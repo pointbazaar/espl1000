520b733e32e512841f847affe05ae19d6bd574a1
Alexander Hansen <alex23667@gmail.com>
2023-01-08 15:34:09 +0100

    improve tests for TAC_COPY AVR Codegen and enable TAC_COPY for 16 bit temporaries

--- compiler/main/avr_code_gen/compile_ir/compile_tac_copy.c
+++ compiler/main/avr_code_gen/compile_ir/compile_tac_copy.c
@@ -9,9 +9,11 @@
 
 void compile_tac_copy(struct RAT* rat, struct TAC* tac, struct IBuffer* ibu) {
 
-    const int reg_src = rat_get_register(rat, tac->arg1);
+	const int reg_src = rat_get_register(rat, tac->arg1);
+	const int reg_dest = rat_get_register(rat, tac->dest);
 
-    int reg_dest = rat_get_register(rat, tac->dest);
+	mov(reg_dest, reg_src, "TAC_COPY");
 
-    mov(reg_dest, reg_src, "TAC_COPY");
+	if(rat_is_wide(rat, tac->arg1))
+		mov(reg_dest+1, reg_src+1, "TAC_COPY");
 }
