8e51724dcde3718c45f14fbef1270175cefff334
Alexander Hansen <alex23667@gmail.com>
2023-01-08 15:02:41 +0100

    improve the tests for AVR Codegen for TAC_CONST_VALUE to test more values

--- compiler/test/avr_code_gen/compile_ir/test_compile_tac_const_value.c
+++ compiler/test/avr_code_gen/compile_ir/test_compile_tac_const_value.c
@@ -19,70 +19,70 @@ static void test_8bit();
 static void test_16bit();
 
 void test_compile_tac_const_value(){
-	
+
 	test_8bit();
 	test_16bit();
 }
 
 static void test_8bit(){
-	
+
 	status_test_codegen("TAC_CONST_VALUE - 8 bit");
-	
-	const int8_t fixed_value = rand()%0xff;
 
-    struct TACBuffer* b = tacbuffer_ctor();
-    
-    tacbuffer_append(b, makeTACConst(1, fixed_value));
-    tacbuffer_append(b, makeTACReturn(1));
+	for(int8_t fixed_value = -100; fixed_value < 100; fixed_value += 10){
 
-    vmcu_system_t* system = prepare_vmcu_system_from_tacbuffer(b);
+		struct TACBuffer* b = tacbuffer_ctor();
 
-    vmcu_system_step_n(system, 10);
-        
-	//check that the value was written to any register
-	bool found = false;
-	
-	for(int i = 0; i < 32; i++){
-		
-		if(vmcu_system_read_gpr(system, i) == fixed_value) found = true;
+		tacbuffer_append(b, makeTACConst(1, fixed_value));
+
+		vmcu_system_t* system = prepare_vmcu_system_from_tacbuffer(b);
+
+		vmcu_system_step_n(system, 10);
+
+		//check that the value was written to any register
+		bool found = false;
+
+		for(int i = 0; i < 32; i++){
+
+			if(vmcu_system_read_gpr(system, i) == fixed_value) found = true;
+		}
+
+		assert(found);
+
+		vmcu_system_dtor(system);
 	}
-	
-	assert(found);
-	
-	vmcu_system_dtor(system);
 }
 
 static void test_16bit(){
-	
+
 	status_test_codegen("TAC_CONST_VALUE - 16 bit");
-	
-	const int16_t fixed_value = 300 + (rand()%0xff);
 
-    struct TACBuffer* b = tacbuffer_ctor();
-    
-    tacbuffer_append(b, makeTACConst16(1, fixed_value));
-    tacbuffer_append(b, makeTACReturn(1));
+	for(int16_t fixed_value = -30000; fixed_value < 30000; fixed_value += 2000){
 
-    vmcu_system_t* system = prepare_vmcu_system_from_tacbuffer(b);
+		struct TACBuffer* b = tacbuffer_ctor();
 
-    vmcu_system_step_n(system, 10);
-        
-	//check that the value was written to any register,
-	//and the upper half to the reg above it
-	bool found = false;
-	
-	for(int i = 0; i < 32; i++){
-		
-		if(vmcu_system_read_gpr(system, i) == (int8_t)(fixed_value & 0xff)){
-			found = true;
-			
-			int8_t rn = vmcu_system_read_gpr(system, i+1);
-			
-			assert(rn == (int8_t)((fixed_value >> 8) & 0xff));
+		tacbuffer_append(b, makeTACConst16(1, fixed_value));
+
+		vmcu_system_t* system = prepare_vmcu_system_from_tacbuffer(b);
+
+		vmcu_system_step_n(system, 10);
+
+		//check that the value was written to any register,
+		//and the upper half to the reg above it
+		bool found = false;
+
+		for(int i = 0; i < 32; i++){
+
+			if(vmcu_system_read_gpr(system, i) == (int8_t)(fixed_value & 0xff)){
+				found = true;
+
+				int8_t rn = vmcu_system_read_gpr(system, i+1);
+
+				assert(rn == (int8_t)((fixed_value >> 8) & 0xff));
+			}
 		}
+
+		assert(found);
+
+		vmcu_system_dtor(system);
 	}
-	
-	assert(found);
-	
-	vmcu_system_dtor(system);
 }
