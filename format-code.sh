c0165f42268f3b1f9d558a024ee2486889c70264
Alexander Hansen <alex23667@gmail.com>
2023-09-09 22:49:18 +0200

    upgraded .clang-format
    more specific formatting rules.
    
    Signed-off-by: Alexander Hansen <alex23667@gmail.com>

--- format-code.sh
+++ format-code.sh
@@ -1,1 +1,11 @@
-clang-format -i --style='file:.clang-format' rat/*.c
+SCRIPT_DIR=$(realpath $(dirname $0))
+
+echo "running from" $SCRIPT_DIR
+
+SRC=$(find $SCRIPT_DIR -type d \( -name 'build' -o -name 'dependencies' \) -prune -o \( -name '*.c' -o -name '*.h' \) -print)
+
+#SRC=$(find rat token -type f \( -name "*.c" -o -name "*.h" \))
+
+#echo $SRC
+
+clang-format -i --style='file:.clang-format' $SRC
