e9bf427b3ba410bbc19821ec0a2ae7e1fa091a73
Alexander Hansen <alex23667@gmail.com>
2023-09-10 00:54:29 +0200

    ci: check .clang-format
    make ci
    
    make check-format
    
    Signed-off-by: Alexander Hansen <alex23667@gmail.com>

---
+++ check-format.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eux
+
+./format-code.sh
+
+format_exit_code=$?
+
+if [ $format_exit_code -ne 0 ]; then
+    exit 1
+fi
+
+dirty=$(git ls-files --modified)
+
+if [[ $dirty ]]; then
+	echo "check-format has failed"
+	echo $dirty
+
+	echo "diff:"
+	diff=$(git diff)
+	echo $diff
+
+	exit 1
+fi
