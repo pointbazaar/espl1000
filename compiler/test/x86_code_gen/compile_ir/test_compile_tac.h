401f444ab3a1fcecf1d75b7b292b301d59023aa8
Alexander Hansen <alex23667@gmail.com>
2024-08-25 02:09:38 +0200

    test: compiler: x86: TAC_PARAM

--- compiler/test/x86_code_gen/compile_ir/test_compile_tac.h
+++ compiler/test/x86_code_gen/compile_ir/test_compile_tac.h
@@ -29,3 +29,5 @@ void test_x86_compile_tac_load_local();
 void test_x86_compile_tac_store_local();
 
 void test_x86_compile_tac_return();
+
+void test_x86_compile_tac_param();
---
+++ compiler/test/x86_code_gen/compile_ir/test_compile_tac_param.c
@@ -0,0 +1,62 @@
+#include <assert.h>
+
+#include "x86_code_gen/cg_x86.h"
+#include "x86_code_gen/cg_x86_basic_block.h"
+
+#include "tac/tacbuffer.h"
+
+#include "../test_x86_code_gen.h"
+#include "../test_x86_code_gen_util.h"
+
+static void test_param(uint64_t fixed_value, bool debug);
+
+void test_x86_compile_tac_param() {
+
+	status_test_x86_codegen("TAC_PARAM");
+
+	test_param(0x003c, false);
+	test_param(0x5a00, false);
+	test_param(0x120000, false);
+}
+
+static void test_param(uint64_t fixed_value, bool debug) {
+
+	//test that the value gets pushed
+	//and the stack pointer decrements
+
+	struct TACBuffer* b = tacbuffer_ctor();
+
+	tacbuffer_append(b, makeTACSetupStackframe(TEST_FAKE_STACKFRAME_SIZE));
+	tacbuffer_append(b, makeTACConst(0, fixed_value));
+	tacbuffer_append(b, makeTACConst(1, 0));
+	tacbuffer_append(b, makeTACParam(0, false));
+
+	struct sd_uc_engine* system = sd_uc_engine_from_tacbuffer_v3(b, debug, true);
+
+	uc_err err;
+
+	if (debug) {
+		sd_uc_print_regs(system);
+		sd_uc_print_stack(system);
+	}
+
+	err = sd_uc_emu_start(system, 0, debug);
+
+	uint64_t rsp;
+
+	err = sd_uc_reg_read(system, UC_X86_REG_RSP, &rsp);
+	assert(err == UC_ERR_OK);
+
+	uint64_t read_value;
+	err = sd_uc_mem_read64(system, rsp, &read_value);
+
+	if (debug) {
+		sd_uc_print_regs(system);
+		sd_uc_print_stack(system);
+	}
+
+	//assert that fixed_value is on the stack
+	assert(read_value == fixed_value);
+
+	sd_uc_close(system);
+}
