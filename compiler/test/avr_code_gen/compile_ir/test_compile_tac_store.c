60221fc11a3a8ebc80087c3d7bc40588dbee7fc7
Alexander Hansen <alex23667@gmail.com>
2024-10-03 12:31:47 +0200

    test: avr: start using redzone for test
    create a redzone in memory to detect unintended writes around the target
    address.

--- compiler/test/avr_code_gen/compile_ir/test_compile_tac_store.c
+++ compiler/test/avr_code_gen/compile_ir/test_compile_tac_store.c
@@ -15,9 +15,9 @@
 
 #include "test_compile_tac.h"
 
-static vmcu_system_t* common(uint16_t addr, int16_t value);
+static vmcu_system_t* common(uint16_t addr, int16_t value, uint8_t redzone);
 
-static vmcu_system_t* common(uint16_t addr, int16_t value) {
+static vmcu_system_t* common(uint16_t addr, int16_t value, uint8_t redzone) {
 
 	struct TACBuffer* b = tacbuffer_ctor();
 
@@ -25,54 +25,109 @@ static vmcu_system_t* common(uint16_t addr, int16_t value) {
 	tacbuffer_append(b, makeTACConst(2, addr));
 	tacbuffer_append(b, makeTACStore(2, 1));
 
-	tacbuffer_append(b, makeTACReturn(1));
-
 	vmcu_system_t* system = prepare_vmcu_system_from_tacbuffer(b);
+
+	// fill the area with red zone to detect unintended writes
+	// around the address
+	for (uint16_t a = addr - 2; a < addr + 4; a++) {
+		vmcu_system_write_data(system, a, redzone);
+	}
 
 	vmcu_system_step_n(system, 10);
 
 	return system;
 }
 
-void test_compile_tac_store_case_8bit_value_8bit_addr() {
+static void assert_redzone(vmcu_system_t* system, uint16_t addr, uint8_t width, uint8_t redzone) {
 
-	status_test_codegen("TAC_STORE (8 bit value, 8 bit addr)");
+	const uint16_t addr1 = addr - 1;
+	const uint16_t addr2 = addr + width;
 
-	for (uint16_t addr = 0xc7; addr < 0xcb; addr++) {
+	const uint8_t before = vmcu_system_read_data(system, addr1);
+	const uint8_t after = vmcu_system_read_data(system, addr2);
 
-		for (int8_t fixed_value = 1; fixed_value < 3; fixed_value++) {
-
-			vmcu_system_t* system = common(addr, fixed_value);
-
-			const int8_t stored = vmcu_system_read_data(system, addr);
-
-			assert(stored == fixed_value);
-
-			vmcu_system_dtor(system);
+	if ((before != redzone) || (after != redzone)) {
+		for (uint16_t a = addr1 - 1; a <= addr2 + 1; a++) {
+			const uint8_t value = vmcu_system_read_data(system, a);
+			printf("[0x%x] = 0x%x\n", a, value);
 		}
 	}
+	assert(before == redzone);
+	assert(after == redzone);
 }
 
-void test_compile_tac_store_case_16bit_value_8bit_addr() {
+void test_compile_tac_store_case_8bit_value_8bit_addr_c7() {
 
-	status_test_codegen("TAC_STORE (16 bit value, 8 bit addr)");
+	status_test_codegen("TAC_STORE (8 bit value, 8 bit addr = 0xc7)");
 
-	for (uint16_t addr = 0xc7; addr < 0xcb; addr++) {
+	const uint16_t addr = 0xc7;
+	const uint8_t fixed_value = 0x2;
+	const uint8_t redzone = 0xff;
 
-		for (uint16_t fixed_value = 0x0f00; fixed_value < 0x0f03; fixed_value++) {
+	vmcu_system_t* system = common(addr, fixed_value, redzone);
 
-			vmcu_system_t* system = common(addr, fixed_value);
+	const uint8_t stored = vmcu_system_read_data(system, addr);
 
-			const uint8_t low = vmcu_system_read_data(system, addr);
-			const uint8_t high = vmcu_system_read_data(system, addr + 1);
+	assert_redzone(system, addr, 1, redzone);
 
-			const int16_t stored = (high << 8) | low;
+	vmcu_system_dtor(system);
+}
 
-			assert(stored == fixed_value);
+void test_compile_tac_store_case_8bit_value_8bit_addr_c8() {
 
-			vmcu_system_dtor(system);
-		}
-	}
+	status_test_codegen("TAC_STORE (8 bit value, 8 bit addr = 0xc8)");
+
+	const uint16_t addr = 0xc8;
+	const uint8_t fixed_value = 0x2;
+	const uint8_t redzone = 0xff;
+
+	vmcu_system_t* system = common(addr, fixed_value, redzone);
+
+	const uint8_t stored = vmcu_system_read_data(system, addr);
+
+	assert(stored == fixed_value);
+
+	assert_redzone(system, addr, 1, redzone);
+
+	vmcu_system_dtor(system);
+}
+
+void test_compile_tac_store_case_16bit_value_8bit_addr_c7_f03() {
+
+	status_test_codegen("TAC_STORE (16 bit value = 0xf03, 8 bit addr = 0xc7)");
+
+	const uint8_t redzone = 0xff;
+	const uint16_t addr = 0xc7;
+	const uint16_t fixed_value = 0x0f03;
+
+	vmcu_system_t* system = common(addr, fixed_value, redzone);
+
+	const uint16_t stored = vmcu_system_read_data16(system, addr);
+
+	assert(stored == fixed_value);
+
+	assert_redzone(system, addr, 2, redzone);
+
+	vmcu_system_dtor(system);
+}
+
+void test_compile_tac_store_case_16bit_value_8bit_addr_c8_f03() {
+
+	status_test_codegen("TAC_STORE (16 bit value = 0xf03, 8 bit addr = 0xc8)");
+
+	const uint8_t redzone = 0xff;
+	const uint16_t addr = 0xc7;
+	const uint16_t fixed_value = 0x0f03;
+
+	vmcu_system_t* system = common(addr, fixed_value, redzone);
+
+	const uint16_t stored = vmcu_system_read_data16(system, addr);
+
+	assert(stored == fixed_value);
+
+	assert_redzone(system, addr, 2, redzone);
+
+	vmcu_system_dtor(system);
 }
 
 void test_compile_tac_store_case_8bit_value_16bit_addr() {
@@ -80,10 +135,11 @@ void test_compile_tac_store_case_8bit_value_16bit_addr() {
 	status_test_codegen("TAC_STORE (8 bit value, 16 bit addr)");
 
 	const uint16_t addr = 1200;
+	const uint8_t redzone = 0xff;
 
 	for (int8_t fixed_value = 1; fixed_value < 3; fixed_value++) {
 
-		vmcu_system_t* system = common(addr, fixed_value);
+		vmcu_system_t* system = common(addr, fixed_value, redzone);
 
 		const int8_t stored = vmcu_system_read_data(system, addr);
 
@@ -98,15 +154,13 @@ void test_compile_tac_store_case_16bit_value_16bit_addr() {
 	status_test_codegen("TAC_STORE (16 bit value, 16 bit addr)");
 
 	const uint16_t addr = 1200;
+	const uint8_t redzone = 0xff;
 
 	for (uint16_t fixed_value = 0x0f00; fixed_value < 0x0f03; fixed_value++) {
 
-		vmcu_system_t* system = common(addr, fixed_value);
+		vmcu_system_t* system = common(addr, fixed_value, redzone);
 
-		const uint8_t low = vmcu_system_read_data(system, addr);
-		const uint8_t high = vmcu_system_read_data(system, addr + 1);
-
-		const int16_t stored = (high << 8) | low;
+		const uint16_t stored = vmcu_system_read_data16(system, addr);
 
 		assert(stored == fixed_value);
 
