0edfb7dfc68d490dec7ec303d5de279ddb24ef12
Alexander Hansen <alex23667@gmail.com>
2025-03-14 18:39:13 +0100

    compiler: inline wrapper function invoke_lexer
    It was not doing a lot!

--- compiler/main/invoke/invoke.c
+++ compiler/main/invoke/invoke.c
@@ -1,4 +1,3 @@
-#include "lexer/src/lexer_flags.h"
 #define _XOPEN_SOURCE
 
 #include <stdbool.h>
@@ -9,17 +8,6 @@ int asprintf(char** restrict strp, const char* restrict fmt, ...);
 
 #include "invoke.h"
 #include "parser/main/util/parser.h"
-
-#include "lexer/src/lexer_main.h"
-
-struct TokenList* invoke_lexer(char* filename, bool write_token_file, bool debug) {
-
-	struct LexerFlags flags;
-	flags.write_token_file = write_token_file;
-	flags.filename = filename;
-	flags.debug = debug;
-	return lexer_main(&flags);
-}
 
 struct Namespace* invoke_parser(struct TokenList* list, char* filename_display) {
 
--- compiler/main/invoke/invoke.h
+++ compiler/main/invoke/invoke.h
@@ -7,7 +7,4 @@
 struct Namespace;
 
 // @returns NULL on error
-struct TokenList* invoke_lexer(char* filename, bool write_token_file, bool debug);
-
-// @returns NULL on error
 struct Namespace* invoke_parser(struct TokenList* list, char* filename_display);
